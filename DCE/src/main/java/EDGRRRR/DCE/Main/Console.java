801735e4136ae36cec2a7f3e61fa3debd8d75e57
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-01-17 09:39:03 +0000

    Update Console.java
    Corrected colors being public.
    Corrected methods using colors over settings.

--- DCE/src/main/java/EDGRRRR/DCE/Main/Console.java
+++ DCE/src/main/java/EDGRRRR/DCE/Main/Console.java
@@ -3,25 +3,25 @@ package EDGRRRR.DCE.Main;
 import org.bukkit.ChatColor;
 
 public class Console {
-    public ChatColor infoColour = ChatColor.GREEN;
-    public ChatColor warnColour = ChatColor.YELLOW;
-    public ChatColor severeColour = ChatColor.RED;
-    public ChatColor prefixColour = ChatColor.AQUA;
-    public String prefix = prefixColour + "[DCE v" + App.get().getDescription().getVersion() + "] - ";
+    private ChatColor infoColour = ChatColor.GREEN;
+    private ChatColor warnColour = ChatColor.YELLOW;
+    private ChatColor severeColour = ChatColor.RED;
+    private ChatColor prefixColour = ChatColor.AQUA;
+    private String prefix = prefixColour + "[DCE v" + App.get().getDescription().getVersion() + "] - ";
     private void send(String message) {
         App.get().getServer().getConsoleSender().sendMessage(prefix + message);
     }
     
     public void info(String message) {
-        send(ChatColor.GREEN + message);
+        send(infoColour + message);
     }
 
     public void warn(String message) {
-        send(ChatColor.YELLOW + message);
+        send(warnColour + message);
     }
 
     public void severe(String message) {
-        send(ChatColor.RED + message);
+        send(severeColour + message);
     }
 
 }
