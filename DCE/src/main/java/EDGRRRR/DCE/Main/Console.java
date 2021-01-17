b04a73095412d1737ea73cd1a347b0922565ab3b
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-01-17 11:23:50 +0000

    Update Console.java
    Added ability to send messages to the console, and to the player using special java function name reusing. ¯\_(ツ)_/¯
    
    Seperated prefix and consolePrefix (conPrefix). Which removes the version number from the user message, QOL.

--- DCE/src/main/java/EDGRRRR/DCE/Main/Console.java
+++ DCE/src/main/java/EDGRRRR/DCE/Main/Console.java
@@ -1,27 +1,84 @@
 package EDGRRRR.DCE.Main;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 
 public class Console {
     private ChatColor infoColour = ChatColor.GREEN;
     private ChatColor warnColour = ChatColor.YELLOW;
     private ChatColor severeColour = ChatColor.RED;
     private ChatColor prefixColour = ChatColor.AQUA;
-    private String prefix = prefixColour + "[DCE v" + App.get().getDescription().getVersion() + "] - ";
+    private String prefix = prefixColour + "[DCE] - ";
+    private String conPrefix = prefixColour + "[DCE v" + App.get().getDescription().getVersion() + "] - ";
+
+    // CONSOLE COMMANDS
+    /**
+     * Sends a message to the console
+     * @param message
+     */
     private void send(String message) {
-        App.get().getServer().getConsoleSender().sendMessage(prefix + message);
+        App.get().getServer().getConsoleSender().sendMessage(conPrefix + message);
     }
     
+    /**
+     * Sends a (default green) message to the console
+     * @param message
+     */
     public void info(String message) {
         send(infoColour + message);
     }
 
+    /**
+     * Sends a (default yellow) message to the console
+     * @param message
+     */
     public void warn(String message) {
         send(warnColour + message);
     }
 
+    /**
+     * Sends a (default red) message to the console
+     * @param message
+     */
     public void severe(String message) {
         send(severeColour + message);
     }
+
+    // PLAYER
+    /**
+     * Sends a message to a player
+     * @param player
+     * @param message
+     */
+    private void send(CommandSender player, String message) {
+        player.sendMessage(prefix + message);
+    }
+
+    /**
+     * Sends an info message to a player
+     * @param player
+     * @param message
+     */
+    public void info(CommandSender player, String message){
+        send(player, infoColour + message);
+    }
+
+    /**
+     * Sends a warning message to a player
+     * @param player
+     * @param message
+     */
+    public void warn(CommandSender player, String message){
+        send(player, warnColour + message);
+    }
+    /**
+     * Sends a severe message to a player
+     * @param player
+     * @param message
+     */
+    public void severe(CommandSender player, String message){
+        send(player, severeColour + message);
+    }
+
 
 }
