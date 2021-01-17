b7550039b6e97d7bccacf86141fdf975386cbbb1
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-01-17 16:12:04 +0000

    Update Balance.java
    Console now uses Player() over CommandSender() for compat.
    Comments!

--- DCE/src/main/java/EDGRRRR/DCE/Main/commands/Balance.java
+++ DCE/src/main/java/EDGRRRR/DCE/Main/commands/Balance.java
@@ -4,21 +4,28 @@ import static EDGRRRR.DCE.Main.App.getCon;
 import static EDGRRRR.DCE.Main.App.getEco;
 
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.command.CommandExecutor;
 
+/**
+ * A command executor class for replying to /balance
+ */
 public class Balance implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // Ensure sender is player or return
         if (!(sender instanceof Player)) {
             return true;
         }
 
+        // Create player object
         Player player = (Player) sender;
 
-        getCon().info(sender, "Balance: £" + getEco().getBalance(player));
+        // Reply to player with their balance.
+        getCon().info(player, "Balance: £" + getEco().getBalance(player));
+        // Graceful exit
         return true;
-    }  
+    }
 }
