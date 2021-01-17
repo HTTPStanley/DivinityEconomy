baf0489820b9f8b96c84f68123d17c7b8983b723
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-01-17 16:14:20 +0000

    Update EconomyM.java
    Added minSendAmount - will be migrated to a config file at some point.
    Comments.
    Added addCash, remCash, sendCash

--- DCE/src/main/java/EDGRRRR/DCE/Main/EconomyM.java
+++ DCE/src/main/java/EDGRRRR/DCE/Main/EconomyM.java
@@ -12,6 +12,9 @@ public class EconomyM {
     // Stores the Vault economy object
     private static Economy economy = null;
 
+    // Settings
+    private static Double minSendAmount = 0.01;
+
 
     /**
      * Sets up the vault economy object
@@ -21,9 +24,9 @@ public class EconomyM {
     public boolean setupEconomy() {
         // Get the app
         App.get();
-        
+
         // Look for vault
-        if (App.get().getServer().getPluginManager().getPlugin("Vault") == null) {            
+        if (App.get().getServer().getPluginManager().getPlugin("Vault") == null) {
             App.getCon().severe("No plugin 'Vault' detected.");
             return false;
         } else {
@@ -58,7 +61,86 @@ public class EconomyM {
      * @return Double
      */
     public Double getBalance(Player player) {
-        return getEconomy().getBalance(player);
+        return economy.getBalance(player);
     }
 
+    /**
+     * Adds <amount> from <player>
+     * @param player
+     * @param amount
+     */
+    public void addCash(Player player, Double amount) {
+        economy.depositPlayer(player, amount);
+    }
+
+    /**
+     * Removes <amount> from <player>
+     * @param player
+     * @param amount
+     */
+    public void remCash(Player player, Double amount) {
+        economy.withdrawPlayer(player, amount);
+    }
+
+    /**
+     * Moves cash <amount> from <from> to <to>
+     * Has a series of checks to ensure this is possible, also sends messages regarding the transfer
+     * @param from
+     * @param to
+     * @param amount
+     */
+    public void sendCash(Player from, Player to, Double amount) {
+        // Get App
+        App.get();
+
+        // Stores if checks completed successfully
+        boolean success = true;
+        // Stores reason for failure
+        String reason = "";
+
+        // Stores repeated information - eases readibility and read calls.
+        Double fromBal = getBalance(from);
+        String fromName = from.getName();
+        String toName = to.getName();
+
+        // Check <from> user has enough cash
+        if (fromBal < amount) {
+            // Set the reason and failure
+            reason = "you only have £" + fromBal;
+			success = false;
+        }
+        // Check <amount> is greater than minSendAmount
+        else if (amount < minSendAmount) {
+            // Set the reason and failure
+            reason = "the minimum send amount is £" + minSendAmount;
+            success = false;
+        }
+
+        if (success == true) {
+            try {
+                // Withdraw <amount> from <from>
+                economy.withdrawPlayer(from, amount);
+                // Deposity <amount> to <to>
+                economy.depositPlayer(to, amount);
+                // Send message to <from>
+                Double newFromBalance = getBalance(from);
+                Double newToBalance = getBalance(to);
+                App.getCon().info(from, "Successfully sent £" + amount + " to " + toName + ". Your new balance is £" + newFromBalance);
+                // Send message to <to>
+                App.getCon().info(to, "Received £" + amount + " from " + fromName + ". Your new balance is £" + newToBalance);
+                // Make Log
+                App.getCon().info(fromName + " sent £" + amount + " to " + toName + ". Their new balances are £" + newFromBalance + " | £" + newToBalance + " respectively.");
+            } catch(Exception e) {
+                // If an error ocurred. Log it.
+                App.getCon().severe("A transaction error occurred when "+ fromName + " tried to send £" + amount + " to " + toName);
+                App.getCon().severe("Error: " + e);
+                // Perhaps try reset bank values, but if an error occurred in this situ it's likely to reoccur.
+            }
+        } else {
+            App.getCon().warn(from, "Cannot send £" + amount + " to " + toName + " as " + reason);
+        }
+        // Return
+        return;
+
+    }
 }
