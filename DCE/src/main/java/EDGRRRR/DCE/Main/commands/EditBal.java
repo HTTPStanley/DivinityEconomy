b4328b503245a5719bc56ffd81c918798d248b88
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-01-19 19:28:34 +0000

    Update EditBal.java
    Updated to follow new style.
    Trim whitespace & Organized imports.
    Command now supports <amount> and <player> <amount>
    Added console log
    Improved messages

--- DCE/src/main/java/EDGRRRR/DCE/Main/commands/EditBal.java
+++ DCE/src/main/java/EDGRRRR/DCE/Main/commands/EditBal.java
@@ -1,18 +1,20 @@
 package EDGRRRR.DCE.Main.commands;
 
-import EDGRRRR.DCE.Main.App;
-import net.milkbowl.vault.economy.EconomyResponse;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import EDGRRRR.DCE.Main.App;
+import net.milkbowl.vault.economy.EconomyResponse;
+import net.milkbowl.vault.economy.EconomyResponse.ResponseType;
 
 /**
  * Command executor for editing (adding or removing) cash to a player
  */
 public class EditBal implements CommandExecutor {
     private App app;
+    private String usage = "/editbal <username> <amount> or /editbal <amount>";
 
     public EditBal(App app) {
         this.app = app;
@@ -27,47 +29,68 @@ public class EditBal implements CommandExecutor {
 
         Player from = (Player) sender;
 
-        // Ensure two or more args
-        if (!(args.length == 2)) {
-            app.getCon().warn(from, "Incorrect usage, see¬");
-            return false;
+        // Use case scenarios
+        // command <amount> - applies amount to self
+        // command <player> <amount> - applies amount to player
+        Player to = null;
+        Double amount = null;
+
+        switch (args.length) {
+            case 0:
+                // No args
+                app.getCon().usage(from, "Not enough arguments.", usage);
+                return true;
+
+            case 1:
+                // use case #1
+                to = from;
+                amount = Double.parseDouble(args[0]);
+                break;
+
+            case 2:
+                // use case #2
+                to = app.getServer().getPlayer(args[0]);
+                amount = Double.parseDouble(args[1]);
+                break;
         }
 
-        // String type of transaction
-        String transType = "";
-        // First arg should be the <to> players name
-        String toName = args[0];
-        // Second arg should be the <amount>
-        double amount = Double.parseDouble(args[1]);
-
-        // Get player by <to> name
-        Player to = app.getServer().getPlayer(toName);
-
-        // Ensure player found or just not null in general.
+        // Ensure to player exists
         if (to == null) {
-            app.getCon().warn(from, "Incorrect player name '" + toName + "', see¬");
-            return false;
+            app.getCon().usage(from, "Incorrect player name.", usage);
+            return true;
         }
-        
-        // If amount is negative, will removeCash and set amount to -amount (since removing x cash deducts x from y. deducting -x from y adds x to y.)
-        // If amount is positive, will deposit cash to account
+
+        // Ensure amount is not null
+        if (amount == null) {
+            app.getCon().usage(from, "Incorrect amount.", usage);
+            return true;
+        }
+
+        // Initializing before because VSCODE IS FUCKING WHINING ABOUT IT.
         EconomyResponse response = null;
-        if (amount >= 0) {
-            transType = "deposited";
+        String transType = null;
+        // Edit cash
+        if (amount > 0){
             response = app.getEco().addCash(to, amount);
-        } else {
-            amount = -amount;
-            transType = "withdrawn";
+            transType = "deposited to";
+        } else if (amount < 0) {
             response = app.getEco().remCash(to, amount);
-        }   
-        
-        if (response.transactionSuccess() == true) {
-            // Messages
-            app.getCon().info(from, "You have " + transType + " £" + response.amount + " to " + to.getName() + "'s account.");
-            app.getCon().info(to, "£" + response.amount + " was " + transType + " to your account. Your new balance is £" + response.balance);
+            transType = "withdrawn from";
         } else {
-            app.getCon().warn(from, "An issue occurred. " + toName + "'s balance remains £" + response.balance);
-        }        
+            response = new EconomyResponse(amount, app.getEco().getBalance(to), ResponseType.FAILURE, "No amount to add or remove.");
+        }
+
+        if (response.transactionSuccess() == true) {
+            if (!(from == to)) {
+                app.getCon().info(from, "You have " + transType + to.getName() + "'s account. New Balance: £" + response.balance);
+            }
+            app.getCon().info(to, "Your balance was " + transType + " by " + from.getName() + " the amount of £" + response.amount + ". New Balance: £" + response.balance);
+            app.getCon().info("Transaction: " + from.getName() + "(£" + app.getEco().getBalance(from) + ") -->  £" + response.amount + " -->" + to.getName() + "(£" + app.getEco().getBalance(to) + ")");
+
+        } else {
+            app.getCon().warn(from, "An issue occurred. " + to.getName() + "'s balance remains £" + response.balance);
+            app.getCon().warn("Transaction error: " + response.errorMessage);
+        }
 
         // Graceful exit
         return true;
