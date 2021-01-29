918025c5948872c3a0cb9bf4c7b60052325fc8a7
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-01-29 17:37:50 +0000

    Update Console.java
    Updated to use config.

--- DCE/src/main/java/EDGRRRR/DCE/Main/Console.java
+++ DCE/src/main/java/EDGRRRR/DCE/Main/Console.java
@@ -1,5 +1,7 @@
 package EDGRRRR.DCE.Main;
 
+import java.util.HashMap;
+
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
@@ -8,19 +10,74 @@ import org.bukkit.entity.Player;
  */
 public class Console {
     private App app;
-    private boolean debugMode = true;
-    private ChatColor infoColour = ChatColor.GREEN;
-    private ChatColor warnColour = ChatColor.YELLOW;
-    private ChatColor severeColour = ChatColor.RED;
-    private ChatColor debugColour = ChatColor.DARK_PURPLE;
-    private ChatColor prefixColour = ChatColor.AQUA;
+
+    // Settings
+    private boolean debugMode;
+    private ChatColor infoColour;
+    private ChatColor warnColour;
+    private ChatColor severeColour;
+    private ChatColor debugColour;
+    private ChatColor prefixColour;
+    private ChatColor prefixSepColour;
     private String prefix;
     private String conPrefix;
 
+    // Colours
+    private HashMap<String, ChatColor> colourMap;
+
     public Console(App app){
         this.app = app;
-        this.prefix = prefixColour + "[DCE] - ";
-        this.conPrefix = prefixColour + "[DCE v" + app.getDescription().getVersion() + "] - ";
+
+        // Colours :D
+        this.colourMap = new HashMap<String, ChatColor>();
+        this.colourMap.put("AQUA", ChatColor.AQUA);
+        this.colourMap.put("BLACK", ChatColor.BLACK);
+        this.colourMap.put("BLUE", ChatColor.BLUE);
+        this.colourMap.put("DARK_AQUA", ChatColor.DARK_AQUA);
+        this.colourMap.put("DARK_BLUE", ChatColor.DARK_BLUE);
+        this.colourMap.put("DARK_GRAY", ChatColor.DARK_GRAY);
+        this.colourMap.put("DARK_GREEN", ChatColor.DARK_GREEN);
+        this.colourMap.put("DARK_PURPLE", ChatColor.DARK_PURPLE);
+        this.colourMap.put("DARK_RED", ChatColor.DARK_RED);
+        this.colourMap.put("GOLD", ChatColor.GOLD);
+        this.colourMap.put("GRAY", ChatColor.GRAY);
+        this.colourMap.put("GREEN", ChatColor.GREEN);
+        this.colourMap.put("LIGHT_PURPLE", ChatColor.LIGHT_PURPLE);
+        this.colourMap.put("MAGIC", ChatColor.MAGIC);
+        this.colourMap.put("RED", ChatColor.RED);
+        this.colourMap.put("WHITE", ChatColor.WHITE);
+        this.colourMap.put("YELLOW", ChatColor.YELLOW);
+
+
+        // Get settings
+        this.debugMode = (app.getConfig().getBoolean(app.getConf().strChatDebug) || app.getConfig().getBoolean(app.getConf().strMainDebugMode));
+        this.infoColour = getColour(app.getConf().strChatInfClr);
+        this.warnColour = getColour(app.getConf().strChatWrnClr);
+        this.severeColour = getColour(app.getConf().strChatSvrClr);
+        this.debugColour = getColour(app.getConf().strChatDbgClr);
+        this.prefixColour = getColour(app.getConf().strChatPfxClr);
+        this.prefixSepColour = getColour(app.getConf().strChatPfxSepClr);
+        String prefix = app.getConfig().getString(app.getConf().strChatMsgPfx);
+        String conPrefix = app.getConfig().getString(app.getConf().strChatConsPfx).replace("%V", app.getDescription().getVersion());
+        String prefixSep = app.getConfig().getString(app.getConf().strChatPfxSep);
+        this.prefix = prefixColour + prefix + prefixSepColour + prefixSep;
+        this.conPrefix = prefixColour + conPrefix + prefixSepColour + prefixSep;
+    }
+
+    /**
+     * Resolves string colours to the corresponding ChatColor object
+     * @param optionName - The config option name
+     * @return
+     */
+    private ChatColor getColour(String optionName) {
+        String option = app.getConfig().getString(optionName);
+        String defaultOption = app.getConfig().getDefaults().getString(optionName);
+        ChatColor colour = colourMap.get(option);
+        if (colour == null) {
+            colour = colourMap.get(defaultOption);
+        }
+
+        return colour;
     }
 
     // CONSOLE COMMANDS
@@ -31,7 +88,7 @@ public class Console {
     private void send(String message) {
         app.getServer().getConsoleSender().sendMessage(conPrefix + message);
     }
-    
+
     /**
      * Sends a (default green) message to the console
      * @param message
@@ -44,7 +101,7 @@ public class Console {
      * Sends a (default green) message to the console
      * @param message
      */
-    public void debug(String message) {        
+    public void debug(String message) {
         if (debugMode == true) {
             send(debugColour + message);
         }
