3dc42bff9f8fcbdb2dcdb85565adef88e1023bba
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-02-05 18:13:39 +0000

    Create MaterialPotionData.java
    Maybe temporary, we'll see..

---
+++ DCE/src/main/java/EDGRRRR/DCE/Economy/Materials/MaterialPotionData.java
@@ -0,0 +1,14 @@
+package EDGRRRR.DCE.Economy.Materials;
+
+public class MaterialPotionData {
+    private boolean extended;
+    private String type;
+    private boolean upgraded;
+
+    public MaterialPotionData(boolean extended, String type, boolean upgraded) {
+        this.extended = extended;
+        this.type = type;
+        this.upgraded = upgraded;        
+    }
+    
+}
