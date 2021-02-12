a0de919b78d1711c7b3da22eaa1109f7bbe15e34
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-02-12 03:47:58 +0000

    Update MaterialPotionData.java
    Updated to support disk saving.

--- DCE/src/main/java/EDGRRRR/DCE/Economy/Materials/MaterialPotionData.java
+++ DCE/src/main/java/EDGRRRR/DCE/Economy/Materials/MaterialPotionData.java
@@ -1,14 +1,56 @@
 package EDGRRRR.DCE.Economy.Materials;
 
-public class MaterialPotionData {
-    private boolean extended;
-    private String type;
-    private boolean upgraded;
+import org.bukkit.configuration.ConfigurationSection;
 
-    public MaterialPotionData(boolean extended, String type, boolean upgraded) {
-        this.extended = extended;
-        this.type = type;
-        this.upgraded = upgraded;        
+/**
+ * A class for storing the potion data section of the config.
+ */
+public class MaterialPotionData {
+    // The configuration section for potionData
+    private ConfigurationSection potionData;
+
+    // The string keys for values
+    private String strExtended = "extended";
+    private String strUpgraded = "upgraded";
+    private String strType = "type";
+
+    /**
+     * Constructor
+     * @param potionData
+     */
+    public MaterialPotionData(ConfigurationSection potionData) {
+        this.potionData = potionData;
     }
-    
+
+    /**
+     * Returns the extended variable
+     * @return
+     */
+    public boolean getExtended() {
+        return potionData.getBoolean(strExtended);
+    }
+
+    /**
+     * Returns the type variable
+     * @return
+     */
+    public String getType() {
+        return potionData.getString(strType);
+    }
+
+    /**
+     * Returns the upgraded variable
+     * @return
+     */
+    public boolean getUpgraded() {
+        return potionData.getBoolean(strUpgraded);
+    }
+
+    /**
+     * Returns the configuration section
+     * @return
+     */
+    public ConfigurationSection getPotionData() {
+        return this.potionData;
+    }
 }
