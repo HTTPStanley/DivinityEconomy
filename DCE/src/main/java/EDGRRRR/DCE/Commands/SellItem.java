ec0d0515b94760dacc28f9ae2c34be51a12a14a4
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-02-16 01:01:29 +0000

    Update SellItem.java
    Added "this" keyword to beginning of all variables that are attributes of the object.
    #10 Fixed receiving wrong amount of cash for sale.

--- DCE/src/main/java/EDGRRRR/DCE/Commands/SellItem.java
+++ DCE/src/main/java/EDGRRRR/DCE/Commands/SellItem.java
@@ -3,7 +3,6 @@ package EDGRRRR.DCE.Commands;
 import EDGRRRR.DCE.Economy.Materials.MaterialData;
 import EDGRRRR.DCE.Main.DCEPlugin;
 import net.milkbowl.vault.economy.EconomyResponse;
-import net.milkbowl.vault.economy.EconomyResponse.ResponseType;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -32,8 +31,8 @@ public class SellItem implements CommandExecutor {
         Player from = (Player) sender;
 
         // Ensure command is enabled
-        if (!(app.getConfig().getBoolean(app.getConf().strComSellItem))) {
-            app.getCon().severe(from, "This command is not enabled.");
+        if (!(this.app.getConfig().getBoolean(this.app.getConf().strComSellItem))) {
+            this.app.getCon().severe(from, "This command is not enabled.");
             return true;
         }
 
@@ -49,28 +48,28 @@ public class SellItem implements CommandExecutor {
             // Material & Amount
             case 2:
                 materialName = args[0];
-                amount = (int) (double) app.getEco().getDouble(args[1]);
+                amount = (int) (double) this.app.getEco().getDouble(args[1]);
                 break;
 
             default:
-                app.getCon().usage(from, "Invalid number of arguments.", usage);
+            this.app.getCon().usage(from, "Invalid number of arguments.", usage);
                 return true;
         }
 
-        MaterialData material = app.getMat().getMaterial(materialName);
+        MaterialData material = this.app.getMat().getMaterial(materialName);
         if (material == null) {
-            app.getCon().usage(from, "Unknown Item: '" + materialName + "'", "");
+            this.app.getCon().usage(from, "Unknown Item: '" + materialName + "'", "");
             return true;
         } else {
-            EconomyResponse priceResponse = app.getMat().getMaterialPrice(material, amount, 1.0, false);
+            EconomyResponse priceResponse = this.app.getMat().getMaterialPrice(material, amount, 1.0, false);
             ItemStack iStack = material.getItemStack(amount);
             if (from.getInventory().contains(iStack)) {
                 from.getInventory().remove(iStack);
                 material.addQuantity(amount);
-                app.getEco().addCash(from, priceResponse.amount);
-                app.getCon().info(from, "You have sold " + amount + " * " + material.getCleanName() + " for £" + app.getEco().round(priceResponse.amount) + ". New balance: £" + app.getEco().round(app.getEco().getBalance(from)));
+                this.app.getEco().addCash(from, priceResponse.balance);
+                this.app.getCon().info(from, "You have sold " + amount + " * " + material.getCleanName() + " for £" + app.getEco().round(priceResponse.balance) + ". New balance: £" + this.app.getEco().round(app.getEco().getBalance(from)));
             } else {
-                app.getCon().usage(from, "You do not have " + amount + " * " + material.getCleanName(), usage);
+                this.app.getCon().usage(from, "You do not have " + amount + " * " + material.getCleanName(), usage);
             }
         }
 
