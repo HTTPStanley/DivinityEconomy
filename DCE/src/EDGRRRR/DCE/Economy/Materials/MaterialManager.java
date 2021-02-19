c0942b96c403f733b8956ef47f32b8b99d54998a
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-02-19 06:01:47 +0000

    Update MaterialManager.java
    material and aliases now final.
    Added several new functions for handling player inventories.
    Comments.
    Corrected integer rounding of getInflation()
    Corrected integer rounding elsewhere.
    Removed IOException throws on loadFile

--- DCE/src/EDGRRRR/DCE/Economy/Materials/MaterialManager.java
+++ DCE/src/EDGRRRR/DCE/Economy/Materials/MaterialManager.java
@@ -1,7 +1,6 @@
 package EDGRRRR.DCE.Economy.Materials;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 
 import org.bukkit.Material;
@@ -29,13 +28,12 @@ public class MaterialManager {
     public int baseTotalMaterials;
 
     // Stores the default items.json file location
-    private String materialsFile = "materials.yml";
-    private String aliasesFile = "aliases.yml";
+    private final String materialsFile = "materials.yml";
+    private final String aliasesFile = "aliases.yml";
 
     /**
      * Constructor You will likely need to call loadMaterials and loadAliases to
-     * populate the aliases and materials with data from the program.
-     *
+     * populate the aliases and materials with data from the program
      * @param app
      */
     public MaterialManager(DCEPlugin app) {
@@ -51,8 +49,8 @@ public class MaterialManager {
     /**
      * Returns a material from the materialData HashMap Will be none if no alias or
      * direct name is found.
-     * @param alias
-     * @return
+     * @param alias - The alias or name of the material to get.
+     * @return MaterialData - Returns the material data corresponding to the string supplied.
      */
     public MaterialData getMaterial(String alias) {
         // Check aliases
@@ -61,10 +59,136 @@ public class MaterialManager {
         String matID = aliases.get(alias);
         if (matID == null)
             matID = alias;
-        MaterialData material = this.materials.get(matID);
-        return material;
+        return this.materials.get(matID);
     }
 
+    /**
+     * Removes the specified number of materials from the players inventory
+     * @param player - The player to remove from
+     * @param material - The material to remove
+     * @param amount - The amount to remove
+     */
+    public void removeMaterialsFromPlayer(Player player, Material material, int amount) {
+        ItemStack[] itemStacks = this.getMaterialSlots(player, material);
+        this.removeMaterialsFromPlayer(itemStacks, amount);
+    }
+
+    /**
+     * Removes the specified number of materials from the players inventory
+     * The players inventory is parsed via itemStacks
+     * @param itemStacks - The players inventory
+     * @param amount - The amount to remove
+     */
+    public void removeMaterialsFromPlayer(ItemStack[] itemStacks, int amount) {
+        int amountLeft = amount;
+        for (ItemStack itemStack : itemStacks) {
+            int stackAmount = itemStack.getAmount();
+            int amountRemoved = 0;
+            if (amountLeft > stackAmount) {
+                amountRemoved = stackAmount;
+                itemStack.setAmount(0);
+            } else {
+                amountRemoved = stackAmount - amountLeft;
+                itemStack.setAmount(amountRemoved);
+            }
+
+            amountLeft -= amountRemoved;
+            if (amountLeft == 0) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * Returns the number of empty slots in a players inventory.
+     * @param player - The player to check
+     * @return int - The number of empty slots
+     */
+    public int getEmptySlots(Player player) {
+        int count = 0;
+        ItemStack[] inventory = player.getInventory().getContents();
+        for (ItemStack iStack : inventory) {
+            if (iStack == null) {
+                count += 1;
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     * Returns the ItemStacks of a specific material in a player
+     * @param player - The player to check
+     * @param material - The material to check
+     * @return ItemStack[] - An array of the ItemStack's in the player of material
+     */
+    public ItemStack[] getMaterialSlots(Player player, Material material) {
+        HashMap<Integer, ? extends ItemStack> inventory = player.getInventory().all(material);
+        ItemStack[] iStacks = new ItemStack[inventory.size()];
+        int idx = 0;
+        for (ItemStack iStack : inventory.values()) {
+            iStacks[idx] = iStack;
+            idx += 1;
+        }
+
+        return iStacks;
+    }
+
+    /**
+     * Returns the total count of a material in a player
+     * @param player - The player to check
+     * @param material - The material to check
+     * @return int - Total count of materials
+     */
+    public int getMaterialCount(Player player, Material material) {
+        return this.getMaterialCount(this.getMaterialSlots(player, material));
+    }
+
+    /**
+     * Calculates the total count of all materials in the ItemStacks
+     * @param iStacks - The array of item stacks
+     * @return int - Total count of materials
+     */
+    public int getMaterialCount(ItemStack[] iStacks) {
+        int count = 0;
+        for (ItemStack iStack : iStacks) {
+            count += iStack.getAmount();
+        }
+
+        return count;
+    }
+
+    /**
+     * Calculates the total available space for a material in the players inventory
+     * @param player - The player to check
+     * @param material - The material to check
+     * @return int - The total space that can be further occupied by a material
+     */
+    public int getAvailableSpace(Player player, Material material) {
+        //Get empty slots
+        //Get total slots used by material
+        //Get total count of materials in those slots
+        int emptySlots = this.getEmptySlots(player);
+        ItemStack[] iStacks = this.getMaterialSlots(player, material);
+        int materialCount = this.getMaterialCount(iStacks);
+
+        // Instantiate space
+        // Add the total space occupied by the number of slots filled less the actual space filled
+        // Add the empty slot space
+        int availableSpace = 0;
+        availableSpace += (iStacks.length * material.getMaxStackSize()) - materialCount;
+        availableSpace += emptySlots * material.getMaxStackSize();
+
+        return availableSpace;
+    }
+
+    /**
+     * Adds <amount> of <material> to <player>
+     * @param player - The player to add the material to
+     * @param material - The material to add
+     * @param amount - The amount to add
+     * @return EconomyResponse - ADD DESCRIPTION
+     */
     public EconomyResponse addMaterial(Player player, Material material, int amount) {
         player.getInventory().addItem(new ItemStack(material, amount));
         return new EconomyResponse(1.0, 1.0, ResponseType.SUCCESS, "");
@@ -77,7 +201,7 @@ public class MaterialManager {
      * @param scale     - The scaling to apply to the final price. For example 1.2 =
      *                  20% ontop for tax reasons
      * @param purchase  - Whether this is a user purchase or sale.
-     * @return
+     * @return EconomyResponse - ADD DESCRIPTION
      */
     public EconomyResponse getMaterialPrice(MaterialData materialD, int amount, double scale, boolean purchase) {
         double value = 0;
@@ -118,9 +242,10 @@ public class MaterialManager {
      * Returns the price for an item based on it's stock and the scale to apply
      * Scale of 1.2 = 20% additive Scale of .8 = 20% reduction
      *
-     * @param stock
-     * @param scale
-     * @return
+     * @param stock - The stock of the material
+     * @param scale - The scaling to apply to the price
+     * @param inflation - The level of inflation
+     * @return double - The price of the material
      */
     private double getPrice(int stock, double scale, double inflation) {
         // Price breakdown
@@ -133,23 +258,33 @@ public class MaterialManager {
         // Inflation works by calculating the default total items and dividing it by the new total items
         // This results in an increase in price when there are less items in the market than default
         // Or a decrease in price when there are more items in the market than default
-        return (this.app.getEco().baseQuantity / stock) * scale * inflation;
+        return (this.app.getEco().baseQuantity / (double) stock) * scale * inflation;
     }
 
+    /**
+     * Gets the market-wide level of inflation
+     * @return double - The level of inflation
+     */
     public double getInflation() {
         return this.getInflation(this.baseTotalMaterials, this.totalMaterials);
     }
 
+    /**
+     * Gets the level of inflation based on the parameters supplied
+     * @param baseQuantity - The base quantity of materials in the market
+     * @param actualQuantity - The actual current quantity of materials in the market
+     * @return double - The level of inflation
+     */
     public double getInflation(int baseQuantity, int actualQuantity) {
-        return baseQuantity / actualQuantity;
+        return (double) baseQuantity / (double) actualQuantity;
     }
 
     /**
      * Returns the market price of a product Market price = exact price (scale of
      * 1.0)
      *
-     * @param stock
-     * @return
+     * @param stock - The stock of the product
+     * @return double - The market (sell) price of the product
      */
     public double getMarketPrice(int stock) {
         return this.getPrice(stock, 1.0, this.getInflation());
@@ -159,8 +294,8 @@ public class MaterialManager {
      * Returns the user price of a product User price = price * tax (scale of 1.2
      * default)
      *
-     * @param stock
-     * @return
+     * @param stock - The stock of the product
+     * @return double - The user (buy) price of the product
      */
     public double getUserPrice(int stock) {
         return this.getPrice(stock, this.app.getEco().tax, this.getInflation());
@@ -208,27 +343,24 @@ public class MaterialManager {
     }
 
     /**
-     *
-     * @param file
-     * @return
-     * @throws IOException
+     * Reads and loads the default config
+     * @param file - The filename of the file
+     * @return FileConfiguration - The file config
      */
-    private FileConfiguration loadDefaultConfig(String file) {return YamlConfiguration.loadConfiguration(new InputStreamReader(this.app.getResource(file)));}
+    private FileConfiguration loadDefaultConfig(String file){return YamlConfiguration.loadConfiguration(new InputStreamReader(this.app.getResource(file)));}
 
     /**
-     *
-     * @param file
-     * @return
-     * @throws IOException
+     * Reads and loads the config
+     * @param file - The filename of the file
+     * @return FileConfiguration - The file config
      */
     private FileConfiguration loadConfig(String file) {return YamlConfiguration.loadConfiguration(new File(this.app.getDataFolder(), file));}
 
     /**
      * Loads the default and current config files If the config file is empty or
      * non-existent, it will be overwritten with the default config And returned
-     *
-     * @param file
-     * @return
+     * @param file - The file to load
+     * @return FileConfiguration - The file config
      */
     public FileConfiguration loadFile(String file) {
         // Instantiate default and user config
@@ -257,8 +389,8 @@ public class MaterialManager {
 
     /**
      * Saves a value to a key
-     * @param key
-     * @param value
+     * @param key - The map key
+     * @param value - The map value
      */
     private void setData(String key, Object value) {
         this.config.set(key, value);
@@ -266,7 +398,7 @@ public class MaterialManager {
 
     /**
      * Saves the material data to the config
-     * @param material
+     * @param material - The material to save
      */
     public void saveMaterial(MaterialData material) {
         this.setData(material.getMaterialID(), material.getConfigData());
