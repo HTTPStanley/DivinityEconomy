3c6379c71dd3a3de73e7f9bfa73d20acd285b504
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-02-23 12:59:13 +0000

    Update Balance.java
    Import cleanup

--- DCE/src/EDGRRRR/DCE/Commands/Balance.java
+++ DCE/src/EDGRRRR/DCE/Commands/Balance.java
@@ -1,12 +1,11 @@
 package EDGRRRR.DCE.Commands;
 
+import EDGRRRR.DCE.Main.DCEPlugin;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
-import EDGRRRR.DCE.Main.DCEPlugin;
 
 /**
  * A command executor class for replying to /balance
