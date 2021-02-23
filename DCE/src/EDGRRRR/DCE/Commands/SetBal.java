46149c669323bddafc8de66100b1ceb20d6fcf6e
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-02-23 13:18:42 +0000

    Update SetBal.java
    Import cleanup.
    Fixed being unable to set balance below £1

--- DCE/src/EDGRRRR/DCE/Commands/SetBal.java
+++ DCE/src/EDGRRRR/DCE/Commands/SetBal.java
@@ -3,7 +3,6 @@ package EDGRRRR.DCE.Commands;
 import EDGRRRR.DCE.Main.DCEPlugin;
 import EDGRRRR.DCE.Math.Math;
 import net.milkbowl.vault.economy.EconomyResponse;
-
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -69,51 +68,46 @@ public class SetBal implements CommandExecutor {
         if (to == null && toOff == null){
             this.app.getConsoleManager().usage(from, "Invalid player name.", usage);
         } else {
-            // Ensure amount is not null
-            if (amount < 1) {
-                this.app.getConsoleManager().usage(from, "Incorrect amount.", usage);
+
+            // Set cash
+            EconomyResponse response;
+            String toName;
+            if (!(to == null)) {
+                response = this.app.getEconomyManager().setCash(to, amount);
+                toName = to.getName();
             } else {
+                response = this.app.getEconomyManager().setCash(toOff, amount);
+                toName = toOff.getName();
+            }
 
-                // Set cash
-                EconomyResponse response;
-                String toName;
-                if (!(to == null)) {
-                    response = this.app.getEconomyManager().setCash(to, amount);
-                    toName = to.getName();
-                } else {
-                    response = this.app.getEconomyManager().setCash(toOff, amount);
-                    toName = toOff.getName();
-                }
+            double balance = this.app.getEconomyManager().round(response.balance);
 
-                double balance = this.app.getEconomyManager().round(response.balance);
+            // Response messages
+            switch (response.type) {
+                case SUCCESS:
+                    // If to != from, respond.
+                    if (!(to == from)) {
+                        this.app.getConsoleManager().info(from, "You set " + toName + "'s balance to £" + balance);
+                    }
 
-                // Response messages
-                switch (response.type) {
-                    case SUCCESS:
-                        // If to != from, respond.
-                        if (!(to == from)) {
-                            this.app.getConsoleManager().info(from, "You set " + toName + "'s balance to £" + balance);
-                        }
+                    // If online send message
+                    if (!(to == null)) {
+                        this.app.getConsoleManager().info(to, "Your balance was set to £" + balance + " by " + from.getName());
 
-                        // If online send message
-                        if (!(to == null)) {
-                            this.app.getConsoleManager().info(to, "Your balance was set to £" + balance + " by " + from.getName());
+                        // If offline --
+                    } else {
+                        // Perhaps send an ingame mail message to offlinePlayer ¯\_(ツ)_/¯
+                    }
 
-                            // If offline --
-                        } else {
-                            // Perhaps send an ingame mail message to offlinePlayer ¯\_(ツ)_/¯
-                        }
+                    // Console feedback
+                    this.app.getConsoleManager().info(from.getName() + " set " + toName + "'s balance to £" + balance);
+                    break;
 
-                        // Console feedback
-                        this.app.getConsoleManager().info(from.getName() + " set " + toName + "'s balance to £" + balance);
-                        break;
+                case FAILURE:
+                    this.app.getConsoleManager().usage(from, response.errorMessage, usage);
 
-                    case FAILURE:
-                        this.app.getConsoleManager().usage(from, response.errorMessage, usage);
-
-                    default:
-                        this.app.getConsoleManager().warn("Balance Set error (" + from.getName() + "-->" + toName + "): " + response.errorMessage);
-                }
+                default:
+                    this.app.getConsoleManager().warn("Balance Set error (" + from.getName() + "-->" + toName + "): " + response.errorMessage);
             }
         }
         return true;
