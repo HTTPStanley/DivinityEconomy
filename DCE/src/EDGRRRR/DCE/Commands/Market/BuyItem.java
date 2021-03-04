dada283ca1295199f61aa3e5371d0f3127efe052
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-03-04 11:08:10 +0000

    Update BuyItem.java
    Work on #26
    Removed unnecessary checks.

--- DCE/src/EDGRRRR/DCE/Commands/Market/BuyItem.java
+++ DCE/src/EDGRRRR/DCE/Commands/Market/BuyItem.java
@@ -2,7 +2,7 @@ package EDGRRRR.DCE.Commands.Market;
 
 import EDGRRRR.DCE.Main.DCEPlugin;
 import EDGRRRR.DCE.Materials.MaterialData;
-import EDGRRRR.DCE.Materials.MaterialValue;
+import EDGRRRR.DCE.Materials.MaterialValueResponse;
 import EDGRRRR.DCE.Math.Math;
 import net.milkbowl.vault.economy.EconomyResponse;
 import net.milkbowl.vault.economy.EconomyResponse.ResponseType;
@@ -59,46 +59,41 @@ public class BuyItem implements CommandExecutor {
 
         if (amount < 1) {
             this.app.getConsoleManager().usage(from, "Invalid amount.", this.usage);
+            this.app.getConsoleManager().debug("(BuyItem)Invalid amount: " + amount);
         } else {
             MaterialData materialData = this.app.getMaterialManager().getMaterial(materialName);
             if (materialData == null) {
                 this.app.getConsoleManager().usage(from, "Unknown Item: '" + materialName + "'", "");
+                this.app.getConsoleManager().debug("(BuyItem)Unknown Item: " + materialName);
 
             } else {
-                if (!materialData.getAllowed()) {
-                    this.app.getConsoleManager().usage(from, "Cannot buy " + materialData.getCleanName() + " because it is not allowed to be bought or sold", this.usage);
-                    this.app.getConsoleManager().warn(from.getName() + " couldn't buy " + materialData.getMaterialID() + " because it is not allowed to be bought or sold");
+                int availableSpace = this.app.getPlayerInventoryManager().getAvailableSpace(from, materialData.getMaterial());
+                if (amount > availableSpace) {
+                    this.app.getConsoleManager().usage(from, "You only have space for " + availableSpace + " " + materialData.getCleanName(), this.usage);
+                    this.app.getConsoleManager().debug(from.getName() + " couldn't buy " + materialData.getMaterialID() + " because missing inventory space " + availableSpace + " / " + amount);
 
                 } else {
-                    int availableSpace = this.app.getPlayerInventoryManager().getAvailableSpace(from, materialData.getMaterial());
-                    if (amount > availableSpace) {
-                        this.app.getConsoleManager().usage(from, "You only have space for " + availableSpace + " " + materialData.getCleanName(), this.usage);
-                        this.app.getConsoleManager().info(from.getName() + " couldn't buy " + materialData.getMaterialID() + " because missing inventory space " + availableSpace + " / " + amount);
+                    ItemStack[] itemStacks = this.app.getPlayerInventoryManager().createItemStacks(materialData.getMaterial(), amount);
+                    MaterialValueResponse priceResponse = this.app.getMaterialManager().getBuyValue(itemStacks);
+                    EconomyResponse saleResponse = this.app.getEconomyManager().remCash(from, priceResponse.getValue());
+                    if (saleResponse.type == ResponseType.SUCCESS && priceResponse.getResponseType() == ResponseType.SUCCESS) {
+                        this.app.getPlayerInventoryManager().addItemsToPlayer(from, itemStacks);
+                        materialData.remQuantity(amount);
+
+                        // Handles console, message and mail
+                        this.app.getConsoleManager().logPurchase(from, amount, saleResponse.amount, materialData.getCleanName());
 
                     } else {
-                        ItemStack[] itemStacks = this.app.getPlayerInventoryManager().createItemStacks(materialData.getMaterial(), amount);
-                        MaterialValue priceResponse = this.app.getMaterialManager().getBuyValue(itemStacks);
-                        EconomyResponse saleResponse = this.app.getEconomyManager().remCash(from, priceResponse.getValue());
-                        double cost = this.app.getEconomyManager().round(saleResponse.amount);
-                        double balance = this.app.getEconomyManager().round(saleResponse.balance);
-                        if (saleResponse.type == ResponseType.SUCCESS && priceResponse.getResponseType() == ResponseType.SUCCESS) {
-                            this.app.getPlayerInventoryManager().addItemsToPlayer(from, itemStacks);
-                            materialData.remQuantity(amount);
-                            this.app.getConsoleManager().info(from, "Bought " + amount + " " + materialData.getCleanName() + " for £" + cost + ". New Balance: £" + balance);
-                            this.app.getConsoleManager().info(from.getName() + " Bought " + amount + " " + materialData.getMaterialID() + " for £" + cost);
+                        String errorMessage;
+                        if (saleResponse.type == ResponseType.FAILURE) errorMessage = saleResponse.errorMessage;
+                        else if (priceResponse.getResponseType() == ResponseType.FAILURE) errorMessage = priceResponse.getErrorMessage();
+                        else errorMessage = "¯\\_(ツ)_/¯";
 
-                        } else {
-                            String errorMessage;
-                            if (saleResponse.type == ResponseType.FAILURE) errorMessage = saleResponse.errorMessage;
-                            else if (priceResponse.getResponseType() == ResponseType.FAILURE)
-                                errorMessage = priceResponse.getErrorMessage();
-                            else errorMessage = "¯\\_(ツ)_/¯";
-
-                            this.app.getConsoleManager().usage(from, "Couldn't buy " + amount + " " + materialData.getCleanName() + " for £" + cost + " because " + errorMessage, this.usage);
-                            this.app.getConsoleManager().warn(from.getName() + " couldn't buy " + amount + " " + materialData.getMaterialID() + " for £" + cost + " because " + errorMessage);
-                        }
+                        // Handles console, message and mail
+                        this.app.getConsoleManager().logFailedPurchase(from, amount, saleResponse.amount, materialData.getCleanName(), errorMessage);
                     }
                 }
+
             }
         }
         return true;
