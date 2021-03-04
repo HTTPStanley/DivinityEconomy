555161ff8cf71cbf555ca8d7de31502ba03d1ab9
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-03-04 13:20:25 +0000

    Create ValueResponse.java
    Work on #30 #29 (Updating Responses)

---
+++ DCE/src/EDGRRRR/DCE/Response/ValueResponse.java
@@ -0,0 +1,22 @@
+package EDGRRRR.DCE.Response;
+
+import net.milkbowl.vault.economy.EconomyResponse;
+
+/**
+ * Used for storing the response to the query of the value of an item
+ */
+public class ValueResponse extends Response {
+    // The value of the enchant
+    public final double value;
+
+    /**
+     * Constructor
+     * @param value - The value of the enchant
+     * @param responseType - The type of response
+     * @param errorMessage - The error, if any.
+     */
+    public ValueResponse(double value, EconomyResponse.ResponseType responseType, String errorMessage) {
+        super(responseType, errorMessage);
+        this.value = value;
+    }
+}
