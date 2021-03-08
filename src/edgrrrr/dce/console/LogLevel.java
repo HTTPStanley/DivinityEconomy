6572eaa09c81eaeb07f52ff5e22267ce2e985f1b
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-03-08 16:45:22 +0000

    Create LogLevel.java
    Used for the logging level. Stores colour and priority.

---
+++ src/edgrrrr/dce/console/LogLevel.java
@@ -0,0 +1,55 @@
+package edgrrrr.dce.console;
+
+import edgrrrr.dce.config.Setting;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+public enum LogLevel {
+    DEBUG(0, ChatColor.DARK_PURPLE, Setting.CHAT_DEBUG_COLOR.path()),
+    INFO(1, ChatColor.GREEN, Setting.CHAT_INFO_COLOR.path()),
+    WARNING(2, ChatColor.YELLOW, Setting.CHAT_WARNING_COLOR.path()),
+    SEVERE(3, ChatColor.DARK_RED, Setting.CHAT_SEVERE_COLOR.path()),
+    FATAL(4, ChatColor.RED, Setting.CHAT_FATAL_COLOR.path());
+
+
+
+    private int priority = 0;
+    private ChatColor colour = ChatColor.GRAY;
+    private String colourOption;
+
+    private LogLevel(int priority, ChatColor color, String colourOption) {
+        this.priority = priority;
+        this.colour = color;
+        this.colourOption = colourOption;
+    }
+
+    private void setColour(ChatColor colour) {
+        this.colourOption = colourOption;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public ChatColor getColour() {
+        return colour;
+    }
+
+    public String getColourOption() {
+        return colourOption;
+    }
+
+    public boolean hasPriority(LogLevel over) {
+        return priority > over.priority;
+    }
+
+    public static void loadValuesFromConfig(YamlConfiguration config) {
+        for(LogLevel level : values()) {
+            String value = config.getString(level.getColourOption());
+            try { level.setColour( ChatColor.valueOf(value) ); }
+            catch (Exception e) { } //TODO: Log warning, bad config value
+        }
+    }
+
+
+}
