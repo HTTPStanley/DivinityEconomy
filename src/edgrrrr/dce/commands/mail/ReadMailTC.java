a71304db7bce86b9b0efc86807471866090defa6
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-03-20 20:16:26 +0000

    Create ReadMailTC.java
    Tab completer for read mail. #21

---
+++ src/edgrrrr/dce/commands/mail/ReadMailTC.java
@@ -0,0 +1,53 @@
+package edgrrrr.dce.commands.mail;
+
+import edgrrrr.dce.DCEPlugin;
+import edgrrrr.dce.config.Setting;
+import edgrrrr.dce.mail.MailList;
+import edgrrrr.dce.materials.MaterialData;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class ReadMailTC implements TabCompleter {
+    private final DCEPlugin app;
+
+    public ReadMailTC(DCEPlugin app) {
+        this.app = app;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        // Ensure player
+        if (!(sender instanceof Player) || !(this.app.getConfig().getBoolean(Setting.COMMAND_READ_MAIL_ENABLE_BOOLEAN.path()))) {
+            return null;
+        }
+
+        String[] strings;
+        MailList mailList = this.app.getMailManager().getMailList((Player) sender);
+        switch (args.length) {
+            // 1 arg
+            // return list of page numbers
+            case 1:
+                ArrayList<String> pageNums = new ArrayList<>();
+                for (int pageNum : mailList.getPages(10).keySet()) {
+                    // +1 since readMail interprets numbers as pgnum + 1
+                    pageNums.add(String.valueOf(pageNum + 1));
+                }
+                strings = pageNums.toArray(new String[0]);
+                break;
+
+            // else
+            default:
+                strings = new String[0];
+                break;
+        }
+
+        return Arrays.asList(strings);
+    }
+}
