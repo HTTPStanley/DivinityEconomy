8aaedda5956bf7c7f48e4a43faeea63f79974363
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-03-20 13:57:41 +0000

    Create BalanceTC.java
    Added tab completer for balance. #21

---
+++ src/edgrrrr/dce/commands/money/BalanceTC.java
@@ -0,0 +1,48 @@
+package edgrrrr.dce.commands.money;
+
+import edgrrrr.dce.DCEPlugin;
+import edgrrrr.dce.config.Setting;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BalanceTC implements TabCompleter {
+    private final DCEPlugin app;
+
+    public BalanceTC(DCEPlugin app) {
+        this.app = app;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        // Ensure player
+        if (!(sender instanceof Player) || !(this.app.getConfig().getBoolean(Setting.COMMAND_BALANCE_ENABLE_BOOLEAN.path()))) {
+            return null;
+        }
+
+        String[] playerNames;
+        switch (args.length) {
+            // 0 args
+            // return names of players
+            case 0:
+                playerNames = this.app.getPlayerManager().getOfflinePlayers();
+                break;
+
+            // 1 args
+            // return names of players starting with arg
+            case 1:
+                playerNames = this.app.getPlayerManager().getOfflinePlayers(args[0]);
+                break;
+
+            default:
+                playerNames = new String[0];
+                break;
+        }
+
+        return Arrays.asList(playerNames);
+    }
+}
