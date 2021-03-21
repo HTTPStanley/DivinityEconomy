5eebf39ac4b37d2ec244acdcc590d6ed32504ce3
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-03-21 19:26:12 +0000

    Create EnchantHandValueTC.java
    Added enchant hand value tc #21

---
+++ src/edgrrrr/dce/commands/enchants/EnchantHandValueTC.java
@@ -0,0 +1,61 @@
+package edgrrrr.dce.commands.enchants;
+
+import edgrrrr.dce.DCEPlugin;
+import edgrrrr.dce.config.Setting;
+import edgrrrr.dce.enchants.EnchantData;
+import edgrrrr.dce.math.Math;
+import edgrrrr.dce.utils.ArrayUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EnchantHandValueTC implements TabCompleter {
+    private final DCEPlugin app;
+
+    public EnchantHandValueTC(DCEPlugin app) {
+        this.app = app;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        // Ensure player
+        if (!(sender instanceof Player) || !(this.app.getConfig().getBoolean(Setting.COMMAND_E_HAND_VALUE_ENABLE_BOOLEAN.path()))) {
+            return null;
+        }
+        Player player = (Player) sender;
+        String[] strings;
+        EnchantData enchantData;
+        ItemStack heldItem = this.app.getPlayerInventoryManager().getHeldItemNotNull(player);
+        switch (args.length) {
+            // 1 args
+            // return names of players starting with arg
+            case 1:
+                strings = this.app.getEnchantmentManager().getCompatibleEnchants(heldItem, args[0]);
+                break;
+
+            // 2 args
+            // return max stack size for the material given
+            case 2:
+                enchantData = this.app.getEnchantmentManager().getEnchant(args[0]);
+                int maxLevel = 1;
+                if (enchantData != null) {
+                    maxLevel = heldItem.getEnchantmentLevel(enchantData.getEnchantment());
+                }
+
+                strings = ArrayUtils.strRange(1, maxLevel);
+                break;
+
+            // else
+            default:
+                strings = new String[0];
+                break;
+        }
+
+        return Arrays.asList(strings);
+    }
+}
