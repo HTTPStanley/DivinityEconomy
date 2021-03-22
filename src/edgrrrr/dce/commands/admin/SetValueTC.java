c9fb20de7d1ffe13f0bdada9b052a82036381ff1
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-03-22 16:19:18 +0000

    Create SetValueTC.java
    #18

---
+++ src/edgrrrr/dce/commands/admin/SetValueTC.java
@@ -0,0 +1,44 @@
+package edgrrrr.dce.commands.admin;
+
+import edgrrrr.dce.DCEPlugin;
+import edgrrrr.dce.config.Setting;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A tab completer for the set value command
+ */
+public class SetValueTC implements TabCompleter {
+    private final DCEPlugin app;
+
+    public SetValueTC(DCEPlugin app) {
+        this.app = app;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        // Ensure player
+        if (!(this.app.getConfig().getBoolean(Setting.COMMAND_SET_VALUE_ENABLE_BOOLEAN.path()))) {
+            return null;
+        }
+
+        String[] strings;
+        switch (args.length) {
+            // Args 1
+            // get player names that start with args[0]
+            case 1:
+                strings = this.app.getMaterialManager().getMaterialNames();
+                break;
+
+            default:
+                strings = new String[0];
+                break;
+        }
+
+        return Arrays.asList(strings);
+    }
+}
