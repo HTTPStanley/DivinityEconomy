f6e764546a8ea0d4cdd547eff31c1ee45874e76d
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-03-22 14:48:15 +0000

    Create ReloadEnchants.java
    #36

---
+++ src/edgrrrr/dce/commands/admin/ReloadEnchants.java
@@ -0,0 +1,40 @@
+package edgrrrr.dce.commands.admin;
+
+import edgrrrr.dce.DCEPlugin;
+import edgrrrr.dce.config.Setting;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * A simple ping pong! command
+ */
+public class ReloadEnchants implements CommandExecutor {
+    private final DCEPlugin app;
+
+    public ReloadEnchants(DCEPlugin app) {
+        this.app = app;
+    }
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player;
+        if (sender instanceof Player) {
+            player = (Player) sender;
+        } else {
+            player = null;
+        }
+
+        // Ensure command is enabled
+        if (!(this.app.getConfig().getBoolean(Setting.COMMAND_RELOAD_ENCHANTS_ENABLE_BOOLEAN.path()))) {
+            DCEPlugin.CONSOLE.severe(player, "This command is not enabled.");
+            return true;
+        }
+
+        this.app.getEnchantmentManager().loadEnchants();
+        DCEPlugin.CONSOLE.info(player, "Reloaded Enchants");
+        return true;
+    }
+}
