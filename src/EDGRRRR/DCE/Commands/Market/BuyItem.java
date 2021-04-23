9ea365689580dbe43ccf6c2ea5fe245b422102de
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-04-23 17:12:39 +0100

    Update BuyItem.java
    #48

--- src/EDGRRRR/DCE/Commands/Market/BuyItem.java
+++ src/EDGRRRR/DCE/Commands/Market/BuyItem.java
@@ -2,51 +2,37 @@ package edgrrrr.dce.commands.market;
 
 import edgrrrr.configapi.Setting;
 import edgrrrr.dce.DCEPlugin;
-import edgrrrr.dce.help.Help;
+import edgrrrr.dce.commands.DivinityCommandMarket;
 import edgrrrr.dce.materials.MaterialData;
 import edgrrrr.dce.math.Math;
 import edgrrrr.dce.player.PlayerInventoryManager;
 import edgrrrr.dce.response.ValueResponse;
 import net.milkbowl.vault.economy.EconomyResponse;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 /**
  * A command for buying items from the market
  */
-public class BuyItem implements CommandExecutor {
-    private final DCEPlugin app;
-    private final Help help;
-
+public class BuyItem extends DivinityCommandMarket {
+    /**
+     * Constructor
+     *
+     * @param app
+     */
     public BuyItem(DCEPlugin app) {
-        this.app = app;
-        this.help = this.app.getHelpManager().get("buy");
+        super(app, "buyitem", false, Setting.COMMAND_BUY_ITEM_ENABLE_BOOLEAN);
     }
 
-
+    /**
+     * For handling a player calling this command
+     *
+     * @param sender
+     * @param args
+     * @return
+     */
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            return true;
-        }
-
-        Player player = (Player) sender;
-
-        // Ensure command is enabled
-        if (!(this.app.getConfig().getBoolean(Setting.COMMAND_BUY_ITEM_ENABLE_BOOLEAN.path))) {
-            this.app.getConsole().severe(player, "This command is not enabled.");
-            return true;
-        }
-
-        // Ensure market is enabled
-        if (!(this.app.getConfig().getBoolean(Setting.MARKET_MATERIALS_ENABLE_BOOLEAN.path))) {
-            this.app.getConsole().severe(player, "The market is not enabled.");
-            return true;
-        }
-
+    public boolean onPlayerCommand(Player sender, String[] args) {
         String materialName;
         int amountToBuy = 1;
         switch (args.length) {
@@ -62,53 +48,74 @@ public class BuyItem implements CommandExecutor {
                 break;
 
             default:
-                this.app.getConsole().usage(player, "Invalid number of arguments.", this.help.getUsages());
+                this.app.getConsole().usage(sender, Message.InvalidNumberOfArguments.message, this.help.getUsages());
                 return true;
         }
 
+        // Ensure amount is greater than 0
         if (amountToBuy < 1) {
-            this.app.getConsole().usage(player, "Invalid amount.", this.help.getUsages());
-            this.app.getConsole().debug("(BuyItem)Invalid amount: " + amountToBuy);
-
-        } else {
-            MaterialData materialData = this.app.getMaterialManager().getMaterial(materialName);
-            if (materialData == null) {
-                this.app.getConsole().usage(player, "Unknown Item: '" + materialName + "'", this.help.getUsages());
-                this.app.getConsole().debug("(BuyItem)Unknown Item: " + materialName);
-
-            } else {
-                int availableSpace = PlayerInventoryManager.getAvailableSpace(player, materialData.getMaterial());
-                if (amountToBuy > availableSpace) {
-                    this.app.getConsole().logFailedPurchase(player, amountToBuy, materialData.getCleanName(), String.format("missing inventory space (%d/%d)", availableSpace, amountToBuy));
-
-                } else {
-                    if (amountToBuy > materialData.getQuantity()) {
-                        this.app.getConsole().logFailedPurchase(player, amountToBuy, materialData.getCleanName(), String.format("not enough stock (%d/%d)", materialData.getQuantity(), amountToBuy));
-
-                    } else {
-
-                        ItemStack[] itemStacks = PlayerInventoryManager.createItemStacks(materialData.getMaterial(), amountToBuy);
-                        ValueResponse priceResponse = this.app.getMaterialManager().getBuyValue(itemStacks);
-                        EconomyResponse saleResponse = this.app.getEconomyManager().remCash(player, priceResponse.value);
-                        if (saleResponse.transactionSuccess() && priceResponse.isSuccess()) {
-                            PlayerInventoryManager.addItemsToPlayer(player, itemStacks);
-                            materialData.remQuantity(amountToBuy);
-
-                            // Handles console, message and mail
-                            this.app.getConsole().logPurchase(player, amountToBuy, saleResponse.amount, materialData.getCleanName());
-
-                        } else {
-                            String errorMessage = "unknown error";
-                            if (!saleResponse.transactionSuccess()) errorMessage = saleResponse.errorMessage;
-                            else if (priceResponse.isFailure()) errorMessage = priceResponse.errorMessage;
-
-                            // Handles console, message and mail
-                            this.app.getConsole().logFailedPurchase(player, amountToBuy, materialData.getCleanName(), errorMessage);
-                        }
-                    }
-                }
-            }
+            this.app.getConsole().usage(sender, Message.InvalidAmountGiven.message, this.help.getUsages());
+            return true;
         }
+
+        // Ensure Material given exists.
+        MaterialData materialData = this.app.getMaterialManager().getMaterial(materialName);
+        if (materialData == null) {
+            this.app.getConsole().usage(sender, String.format(Message.InvalidItemName.message, materialName), this.help.getUsages());
+            return true;
+        }
+
+        // Ensure player has the available inventory space
+        int availableSpace = PlayerInventoryManager.getAvailableSpace(sender, materialData.getMaterial());
+        if (amountToBuy > availableSpace) {
+            this.app.getConsole().logFailedPurchase(sender, amountToBuy, materialData.getCleanName(), String.format(Message.InvalidInventorySpace.message, availableSpace, amountToBuy));
+            return true;
+        }
+
+        // Ensure market has enough stock
+        if (amountToBuy > materialData.getQuantity()) {
+            this.app.getConsole().logFailedPurchase(sender, amountToBuy, materialData.getCleanName(), String.format(Message.InvalidStockAmount.message, materialData.getQuantity(), amountToBuy));
+            return true;
+        }
+
+        // Get item stacks to buy
+        // Get the value of the item stacks
+        // Remove the value of the items from the player
+        ItemStack[] itemStacks = PlayerInventoryManager.createItemStacks(materialData.getMaterial(), amountToBuy);
+        ValueResponse priceResponse = this.app.getMaterialManager().getBuyValue(itemStacks);
+        EconomyResponse saleResponse = this.app.getEconomyManager().remCash(sender, priceResponse.value);
+
+        // Handle adding items to player and removing quantity from market
+        if (saleResponse.transactionSuccess() && priceResponse.isSuccess()) {
+            PlayerInventoryManager.addItemsToPlayer(sender, itemStacks);
+            materialData.remQuantity(amountToBuy);
+
+            // Handles console, message and mail
+            this.app.getConsole().logPurchase(sender, amountToBuy, saleResponse.amount, materialData.getCleanName());
+        }
+
+        // If the transaction or valuation failed then the user is returned an error.
+        else {
+            String errorMessage = Message.UnknownError.message;
+            if (!saleResponse.transactionSuccess()) errorMessage = saleResponse.errorMessage;
+            else if (priceResponse.isFailure()) errorMessage = priceResponse.errorMessage;
+
+            // Handles console, message and mail
+            this.app.getConsole().logFailedPurchase(sender, amountToBuy, materialData.getCleanName(), errorMessage);
+        }
+
+
         return true;
+    }
+
+    /**
+     * For the handling of the console calling this command
+     *
+     * @param args
+     * @return
+     */
+    @Override
+    public boolean onConsoleCommand(String[] args) {
+        return false;
     }
 }
