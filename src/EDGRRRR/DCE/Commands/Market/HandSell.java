6cd0d166c7b77717702a54506bcdf44c51a9c29a
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-04-23 18:06:32 +0100

    Update HandSell.java

--- src/EDGRRRR/DCE/Commands/Market/HandSell.java
+++ src/EDGRRRR/DCE/Commands/Market/HandSell.java
@@ -2,52 +2,40 @@ package edgrrrr.dce.commands.market;
 
 import edgrrrr.configapi.Setting;
 import edgrrrr.dce.DCEPlugin;
-import edgrrrr.dce.help.Help;
+import edgrrrr.dce.commands.DivinityCommandMarket;
 import edgrrrr.dce.materials.MaterialData;
 import edgrrrr.dce.math.Math;
 import edgrrrr.dce.player.PlayerInventoryManager;
 import edgrrrr.dce.response.ValueResponse;
 import net.milkbowl.vault.economy.EconomyResponse;
 import org.bukkit.Material;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 /**
  * A command for selling items in the users hand
  */
-public class HandSell implements CommandExecutor {
-    private final DCEPlugin app;
-    private final Help help;
+public class HandSell extends DivinityCommandMarket {
 
+    /**
+     * Constructor
+     *
+     * @param app
+     */
     public HandSell(DCEPlugin app) {
-        this.app = app;
-        this.help = this.app.getHelpManager().get("handsell");
+        super(app, "handsell", false, Setting.COMMAND_HAND_SELL_ITEM_ENABLE_BOOLEAN);
     }
 
-
+    /**
+     * ###To be overridden by the actual command
+     * For handling a player calling this command
+     *
+     * @param sender
+     * @param args
+     * @return
+     */
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            return true;
-        }
-
-        Player player = (Player) sender;
-
-        // Ensure command is enabled
-        if (!(this.app.getConfig().getBoolean(Setting.COMMAND_HAND_SELL_ITEM_ENABLE_BOOLEAN.path))) {
-            this.app.getConsole().severe(player, "This command is not enabled.");
-            return true;
-        }
-
-        // Ensure market is enabled
-        if (!(this.app.getConfig().getBoolean(Setting.MARKET_MATERIALS_ENABLE_BOOLEAN.path))) {
-            this.app.getConsole().severe(player, "The market is not enabled.");
-            return true;
-        }
-
+    public boolean onPlayerCommand(Player sender, String[] args) {
         int amountToSell = -1;
 
         switch (args.length) {
@@ -60,52 +48,76 @@ public class HandSell implements CommandExecutor {
                 break;
 
             default:
-                this.app.getConsole().usage(player, "Invalid number of arguments.", this.help.getUsages());
+                this.app.getConsole().usage(sender, CommandResponse.InvalidNumberOfArguments.message, this.help.getUsages());
                 return true;
         }
 
+        // Ensure amount is above 0
         if (amountToSell < 1) {
-            this.app.getConsole().usage(player, "Invalid amount.", this.help.getUsages());
-        } else {
-            ItemStack heldItem = PlayerInventoryManager.getHeldItem(player);
+            this.app.getConsole().usage(sender, CommandResponse.InvalidAmountGiven.message, this.help.getUsages());
+            return true;
+        }
 
-            if (heldItem == null) {
-                this.app.getConsole().usage(player, "You are not holding any item.", this.help.getUsages());
 
-            } else {
-                Material material = heldItem.getType();
-                String materialName = material.name();
-                MaterialData materialData = this.app.getMaterialManager().getMaterial(materialName);
-                int materialCount = PlayerInventoryManager.getMaterialCount(PlayerInventoryManager.getMaterialSlots(player, material));
+        ItemStack heldItem = PlayerInventoryManager.getHeldItem(sender);
 
-                if (materialCount < amountToSell) {
-                    this.app.getConsole().logFailedSale(player, amountToSell, materialData.getCleanName(), String.format("you do not have enough of this material (%d/%d)", materialCount, amountToSell));
+        // Ensure item held is not null
+        if (heldItem == null) {
+            this.app.getConsole().usage(sender, CommandResponse.InvalidItemHeld.message, this.help.getUsages());
+            return true;
+        }
 
-                } else {
-                    ItemStack[] itemStacks = PlayerInventoryManager.getMaterialSlotsToCount(player, material, amountToSell);
-                    ItemStack[] itemStacksClone = PlayerInventoryManager.cloneItems(itemStacks);
-                    ValueResponse response = this.app.getMaterialManager().getSellValue(itemStacks);
+        Material material = heldItem.getType();
+        String materialName = material.name();
+        MaterialData materialData = this.app.getMaterialManager().getMaterial(materialName);
+        int materialCount = PlayerInventoryManager.getMaterialCount(PlayerInventoryManager.getMaterialSlots(sender, material));
 
-                    if (response.isSuccess()) {
-                        PlayerInventoryManager.removeMaterialsFromPlayer(itemStacks);
+        // Ensure player inventory has enough
+        if (materialCount < amountToSell) {
+            this.app.getConsole().logFailedSale(sender, amountToSell, materialData.getCleanName(), String.format(CommandResponse.InvalidInventoryStock.message, materialCount, amountToSell));
+            return true;
+        }
 
-                        EconomyResponse economyResponse = this.app.getEconomyManager().addCash(player, response.value);
-                        if (!economyResponse.transactionSuccess()) {
-                            PlayerInventoryManager.addItemsToPlayer(player, itemStacksClone);
-                            // Handles console, player message and mail
-                            this.app.getConsole().logFailedSale(player, amountToSell, materialData.getCleanName(), economyResponse.errorMessage);
-                        } else {
-                            materialData.addQuantity(amountToSell);
-                            // Handles console, player message and mail
-                            this.app.getConsole().logSale(player, amountToSell, response.value, materialData.getCleanName());
-                        }
-                    } else {
-                        // Handles console, player message and mail
-                        this.app.getConsole().logFailedSale(player, amountToSell, materialData.getCleanName(), response.errorMessage);
-                    }
-                }
+        // Get item stacks to remove
+        // Clone item stacks incase they need to be refunded
+        // Get the value
+        ItemStack[] itemStacks = PlayerInventoryManager.getMaterialSlotsToCount(sender, material, amountToSell);
+        ItemStack[] itemStacksClone = PlayerInventoryManager.cloneItems(itemStacks);
+        ValueResponse response = this.app.getMaterialManager().getSellValue(itemStacks);
+
+        if (response.isSuccess()) {
+            PlayerInventoryManager.removeMaterialsFromPlayer(itemStacks);
+
+            EconomyResponse economyResponse = this.app.getEconomyManager().addCash(sender, response.value);
+            if (!economyResponse.transactionSuccess()) {
+                PlayerInventoryManager.addItemsToPlayer(sender, itemStacksClone);
+                // Handles console, player message and mail
+                this.app.getConsole().logFailedSale(sender, amountToSell, materialData.getCleanName(), economyResponse.errorMessage);
+            }
+
+            else {
+                materialData.addQuantity(amountToSell);
+                // Handles console, player message and mail
+                this.app.getConsole().logSale(sender, amountToSell, response.value, materialData.getCleanName());
             }
         }
+
+        else {
+            // Handles console, player message and mail
+            this.app.getConsole().logFailedSale(sender, amountToSell, materialData.getCleanName(), response.errorMessage);
+        }
         return true;
+    }
+
+    /**
+     * ###To be overridden by the actual command
+     * For the handling of the console calling this command
+     *
+     * @param args
+     * @return
+     */
+    @Override
+    public boolean onConsoleCommand(String[] args) {
+        return false;
     }
 }
