d259ae46978964d9fecfeb67139bc218734d056a
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-04-23 16:06:16 +0100

    Update SendCash.java
    Updated to use DivinityCommand #30

--- src/EDGRRRR/DCE/Commands/Money/SendCash.java
+++ src/EDGRRRR/DCE/Commands/Money/SendCash.java
@@ -2,6 +2,7 @@ package edgrrrr.dce.commands.money;
 
 import edgrrrr.configapi.Setting;
 import edgrrrr.dce.DCEPlugin;
+import edgrrrr.dce.commands.DivinityCommand;
 import edgrrrr.dce.help.Help;
 import edgrrrr.dce.math.Math;
 import edgrrrr.dce.response.EconomyTransferResponse;
@@ -15,30 +16,26 @@ import org.bukkit.entity.Player;
 /**
  * A command for sending cash between players
  */
-public class SendCash implements CommandExecutor {
-    private final DCEPlugin app;
-    private final Help help;
+public class SendCash extends DivinityCommand {
 
+    /**
+     * Constructor
+     *
+     * @param app
+     */
     public SendCash(DCEPlugin app) {
-        this.app = app;
-        this.help = this.app.getHelpManager().get("sendcash");
+        super(app, "sendcash", false, Setting.COMMAND_SEND_CASH_ENABLE_BOOLEAN);
     }
 
+    /**
+     * For handling a player calling this command
+     *
+     * @param sender
+     * @param args
+     * @return
+     */
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // Ensure player
-        if (!(sender instanceof Player)) {
-            return true;
-        }
-        // Cast player
-        Player player1 = (Player) sender;
-
-        // Ensure command is enabled
-        if (!(this.app.getConfig().getBoolean(Setting.COMMAND_SEND_CASH_ENABLE_BOOLEAN.path))) {
-            this.app.getConsole().severe(player1, "This command is not enabled.");
-            return true;
-        }
-
+    public boolean onPlayerCommand(Player sender, String[] args) {
         // Use case scenarios
         // command <player> <amount>
         OfflinePlayer player2;
@@ -47,35 +44,40 @@ public class SendCash implements CommandExecutor {
         switch (args.length) {
             case 2:
                 // Get online player
-                player2 = this.app.getServer().getPlayer(args[0]);
+                player2 = this.app.getPlayerManager().getOfflinePlayer(args[0], false);
                 amount = Math.getDouble(args[1]);
-                // If they aren't online or don't exist.
-                if (player2 == null) {
-                    player2 = this.app.getPlayerManager().getOfflinePlayer(args[0], false);
-                }
                 break;
 
             default:
-                this.app.getConsole().usage(player1, "Invalid number of arguments.", this.help.getUsages());
+                this.app.getConsole().usage(sender, Message.InvalidNumberOfArguments.message, this.help.getUsages());
                 return true;
         }
 
         // Ensure online or offline player exists.
         if (player2 == null) {
-            this.app.getConsole().usage(player1, "Invalid player name.", this.help.getUsages());
+            this.app.getConsole().usage(sender, Message.InvalidNumberOfArguments.message, this.help.getUsages());
 
         } else {
-            EconomyTransferResponse response = this.app.getEconomyManager().sendCash(player1, player2, amount);
+            EconomyTransferResponse response = this.app.getEconomyManager().sendCash(sender, player2, amount);
 
             // Handles console, message and mail
             if (response.responseType == EconomyResponse.ResponseType.SUCCESS) {
-                this.app.getConsole().logTransfer(player1, player2, amount);
+                this.app.getConsole().logTransfer(sender, player2, amount);
             } else {
-                this.app.getConsole().logFailedTransfer(player1, player2, amount, response.errorMessage);
+                this.app.getConsole().logFailedTransfer(sender, player2, amount, response.errorMessage);
             }
         }
-
-        // Graceful exit
         return true;
+    }
+
+    /**
+     * For the handling of the console calling this command
+     *
+     * @param args
+     * @return
+     */
+    @Override
+    public boolean onConsoleCommand(String[] args) {
+        return false;
     }
 }
