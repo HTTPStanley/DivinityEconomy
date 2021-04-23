7be3aa104423ba5d33c56dac134943fe43c24293
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-04-23 16:06:02 +0100

    Update Balance.java
    Updated to use DivinityCommand #34 #30

--- src/EDGRRRR/DCE/Commands/Money/Balance.java
+++ src/EDGRRRR/DCE/Commands/Money/Balance.java
@@ -2,101 +2,96 @@ package edgrrrr.dce.commands.money;
 
 import edgrrrr.configapi.Setting;
 import edgrrrr.dce.DCEPlugin;
-import edgrrrr.dce.help.Help;
+import edgrrrr.dce.commands.DivinityCommand;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 /**
  * A command for getting player's balances
  */
-public class Balance implements CommandExecutor {
-    private final DCEPlugin app;
-    private final Help help;
-    private final Help consoleHelp;
+public class Balance extends DivinityCommand {
 
+    /**
+     * Constructor
+     *
+     * @param app
+     */
     public Balance(DCEPlugin app) {
-        this.app = app;
-        this.help = this.app.getHelpManager().get("balance");
-        this.consoleHelp = new Help("balance", help.getDescription(), help.getAliases(), help.getPermissionNode(), new String[] {"/balance <playername>"});
+        super(app, "balance", true, Setting.COMMAND_BALANCE_ENABLE_BOOLEAN);
     }
 
+    /**
+     * For handling a player calling this command
+     *
+     * @param sender
+     * @param args
+     * @return
+     */
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // Ensure sender is player or return
-        if (sender instanceof Player) {
+    public boolean onPlayerCommand(Player sender, String[] args) {
+        // Use case scenarios
+        // command - returns the callers balance.
+        // command <username> - returns the usernames balance.
+        OfflinePlayer receiverPlayer = null;
 
-            // Create player object
-            Player player1 = (Player) sender;
+        switch (args.length) {
+            case 1:
+                // Get online player
+                receiverPlayer = this.app.getServer().getPlayer(args[0]);
+                // If they aren't online or don't exist. Do the dirty offline call.
+                if (receiverPlayer == null) {
+                    receiverPlayer = this.app.getPlayerManager().getOfflinePlayer(args[0], false);
+                }
+                break;
 
-            // Ensure command is enabled
-            if (!(this.app.getConfig().getBoolean(Setting.COMMAND_BALANCE_ENABLE_BOOLEAN.path))) {
-                this.app.getConsole().severe(player1, "This command is not enabled.");
-                return true;
-            }
-
-            // Use case scenarios
-            // command - returns the callers balance.
-            // command <username> - returns the usernames balance.
-            OfflinePlayer player2 = null;
-
-            switch (args.length) {
-                case 1:
-                    // Get online player
-                    player2 = this.app.getServer().getPlayer(args[0]);
-                    // If they aren't online or don't exist. Do the dirty offline call.
-                    if (player2 == null) {
-                        player2 = this.app.getPlayerManager().getOfflinePlayer(args[0], false);
-                    }
-                    break;
-
-                default:
-                    // any number of args.. just return their own.
-                    player2 = player1;
-                    break;
-            }
-
-            if (player2 == null) {
-                this.app.getConsole().usage(player1, "Invalid player name.", this.help.getUsages());
-                return true;
-            }
-
-            double balance = this.app.getEconomyManager().getBalance(player2);
-            if (!(player1 == player2)) {
-                this.app.getConsole().info(player1, String.format("%s's balance: £%,.2f", player2.getName(), balance));
-            } else {
-                this.app.getConsole().info(player1, String.format("Balance: £%,.2f", balance));
-            }
-        } else {
-            // Ensure command is enabled
-            if (!(this.app.getConfig().getBoolean(Setting.COMMAND_BALANCE_ENABLE_BOOLEAN.path))) {
-                this.app.getConsole().severe("This command is not enabled.");
-                return true;
-            }
-
-            OfflinePlayer player;
-            switch (args.length) {
-                case 1:
-                    player = this.app.getServer().getPlayer(args[0]);
-                    if (player == null) {
-                        this.app.getPlayerManager().getOfflinePlayer(args[0], false);
-                    }
-                    break;
-
-                default:
-                    player = null;
-                    break;
-            }
-
-            if (player == null) {
-                this.app.getConsole().warn("Invalid player name.");
-                return true;
-            }
-
-            this.app.getConsole().info(String.format("%s's balance: £%,.2f", player.getName(), this.app.getEconomyManager().getBalance(player)));
+            default:
+                // any number of args.. just return their own.
+                receiverPlayer = sender;
+                break;
         }
+
+        if (receiverPlayer == null) {
+            this.app.getConsole().usage(sender, Message.InvalidPlayerNameResponse.message, this.help.getUsages());
+            return true;
+        }
+
+        double balance = this.app.getEconomyManager().getBalance(receiverPlayer);
+        if (!(sender == receiverPlayer)) {
+            this.app.getConsole().info(sender, String.format(Message.BalanceResponseOther.message, receiverPlayer.getName(), balance));
+        } else {
+            this.app.getConsole().info(sender, String.format(Message.BalanceResponse.message, balance));
+        }
+        return true;
+    }
+
+    /**
+     * For the handling of the console calling this command
+     *
+     * @param args
+     * @return
+     */
+    @Override
+    public boolean onConsoleCommand(String[] args) {
+        OfflinePlayer player;
+        switch (args.length) {
+            case 1:
+                player = this.app.getServer().getPlayer(args[0]);
+                if (player == null) {
+                    this.app.getPlayerManager().getOfflinePlayer(args[0], false);
+                }
+                break;
+
+            default:
+                this.app.getConsole().send(Message.InvalidNumberOfArguments.defaultLogLevel, Message.InvalidNumberOfArguments.message);
+                return true;
+        }
+
+        if (player == null) {
+            this.app.getConsole().send(Message.InvalidPlayerNameResponse.defaultLogLevel, Message.InvalidPlayerNameResponse.message);
+            return true;
+        }
+
+        this.app.getConsole().info(String.format(Message.BalanceResponseOther.message, player.getName(), this.app.getEconomyManager().getBalance(player)));
         return true;
     }
 }
