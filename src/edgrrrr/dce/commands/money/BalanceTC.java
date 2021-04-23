a1b78135d814125a81ef0a509eda5e17cc2b4449
EDGRRRR <everybodylovesedgrrrr@gmail.com>
2021-04-23 16:06:10 +0100

    Update BalanceTC.java
    Updated to use DivinityCommandTC #34 #30

--- src/edgrrrr/dce/commands/money/BalanceTC.java
+++ src/edgrrrr/dce/commands/money/BalanceTC.java
@@ -2,9 +2,8 @@ package edgrrrr.dce.commands.money;
 
 import edgrrrr.configapi.Setting;
 import edgrrrr.dce.DCEPlugin;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
+import edgrrrr.dce.commands.DivinityCommandTC;
+import org.bukkit.entity.Player;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,20 +11,37 @@ import java.util.List;
 /**
  * A tab completer for the balance command
  */
-public class BalanceTC implements TabCompleter {
-    private final DCEPlugin app;
+public class BalanceTC extends DivinityCommandTC {
 
+    /**
+     * Constructor
+     *
+     * @param app
+     */
     public BalanceTC(DCEPlugin app) {
-        this.app = app;
+        super(app, true, Setting.COMMAND_BALANCE_ENABLE_BOOLEAN);
     }
 
+    /**
+     * For handling a player calling this command
+     *
+     * @param sender
+     * @param args
+     * @return
+     */
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
-        // Ensure player
-        if (!(this.app.getConfig().getBoolean(Setting.COMMAND_BALANCE_ENABLE_BOOLEAN.path))) {
-            return null;
-        }
+    public List<String> onPlayerTabCompleter(Player sender, String[] args) {
+        return this.onConsoleTabCompleter(args);
+    }
 
+    /**
+     * For the handling of the console calling this command
+     *
+     * @param args
+     * @return
+     */
+    @Override
+    public List<String> onConsoleTabCompleter(String[] args) {
         String[] playerNames;
         switch (args.length) {
             // 1 args
