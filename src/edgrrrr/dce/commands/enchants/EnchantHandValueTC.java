7ac7ef8974d50ba7cf8fc97c6b3fb441e9e38f49
EDGRRRRs <everybodylovesedgrrrr@gmail.com>
2021-05-08 10:19:38 +0100

    Update EnchantHandValueTC.java

--- src/edgrrrr/dce/commands/enchants/EnchantHandValueTC.java
+++ src/edgrrrr/dce/commands/enchants/EnchantHandValueTC.java
@@ -3,13 +3,8 @@ package edgrrrr.dce.commands.enchants;
 import edgrrrr.configapi.Setting;
 import edgrrrr.dce.DCEPlugin;
 import edgrrrr.dce.commands.DivinityCommandEnchantTC;
-import edgrrrr.dce.enchants.EnchantData;
-import edgrrrr.dce.player.PlayerInventoryManager;
-import edgrrrr.dce.utils.ArrayUtils;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -35,35 +30,7 @@ public class EnchantHandValueTC extends DivinityCommandEnchantTC {
      */
     @Override
     public List<String> onPlayerTabCompleter(Player sender, String[] args) {
-        String[] strings;
-        EnchantData enchantData;
-        ItemStack heldItem = PlayerInventoryManager.getHeldItemNotNull(sender);
-        switch (args.length) {
-            // 1 args
-            // return names of players starting with arg
-            case 1:
-                strings = this.app.getEnchantmentManager().getCompatibleEnchants(heldItem, args[0]);
-                break;
-
-            // 2 args
-            // return max stack size for the material given
-            case 2:
-                enchantData = this.app.getEnchantmentManager().getEnchant(args[0]);
-                int maxLevel = 1;
-                if (enchantData != null) {
-                    maxLevel = heldItem.getEnchantmentLevel(enchantData.getEnchantment());
-                }
-
-                strings = ArrayUtils.strRange(1, maxLevel);
-                break;
-
-            // else
-            default:
-                strings = new String[0];
-                break;
-        }
-
-        return Arrays.asList(strings);
+        return null;
     }
 
     /**
