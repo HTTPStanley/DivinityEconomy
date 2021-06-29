af55628e2ee4b2cb150be533a425ba87c68eb800
EDGRRRRs <everybodylovesedgrrrr@gmail.com>
2021-06-29 17:04:11 +0100

    Update DEPlugin.java
    Removed obselete loadAliases()/loadMaterials() - Now handled by the DivinityCommand subModule initialisation methods.
    Removed archaic code comment.
    Command registry has moved to DivinityCommand/DivinityCommandTC
    Updated to use new Console methods
    Replaced materials.size() calls with new getCount() methods.

--- src/me/edgrrrr/de/DEPlugin.java
+++ src/me/edgrrrr/de/DEPlugin.java
@@ -30,8 +30,6 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * The Main Class of the plugin
  * Hooks everything together
@@ -59,7 +57,7 @@ public class DEPlugin extends JavaPlugin {
      */
     @Override
     public void onEnable() {
-        // Config
+        // Instantiates all modules
         LogLevel.loadValuesFromConfig((YamlConfiguration) this.getConfig());
         this.config = new ConfigManager(this);
         this.console = new EconConsole(this);
@@ -70,116 +68,90 @@ public class DEPlugin extends JavaPlugin {
         this.mailManager = new MailManager(this);
         this.helpManager = new HelpManager(this);
 
+        // Initialisation of all modules
         DivinityModule.runInit();
-
-        this.materialManager.loadAliases();
-        this.materialManager.loadMaterials();
 
         // setup events
         try {
             // Register events
             PluginManager pm = this.getServer().getPluginManager();
-            // pm.registerEvents(this.getUuidFetchEvent(), this);
             pm.registerEvents(new MailEvent(this), this);
         } catch (Exception e) {
             e.printStackTrace();
-            this.console.severe("An error occurred on event creation: " + e);
+            this.console.severe("An error occurred on event creation: %s", e);
             this.shutdown();
             return;
         }
 
-        try {
-            // Register commands
-            this.getCommand("ping").setExecutor(new Ping(this));
+        // Commands
 
-            this.getCommand("balance").setExecutor(new Balance(this));
-            this.getCommand("balance").setTabCompleter(new BalanceTC(this));
+        // Admin
+        new ClearBal(this);
+        new ClearBalTC(this);
+        new EditBal(this);
+        new EditBalTC(this);
+        new ESetStock(this);
+        new ESetStockTC(this);
+        new ESetValue(this);
+        new ESetValueTC(this);
+        new ReloadEnchants(this);
+        new ReloadMaterials(this);
+        new SaveEnchants(this);
+        new SaveMaterials(this);
+        new SetBal(this);
+        new SetBalTC(this);
+        new SetStock(this);
+        new SetStockTC(this);
+        new SetValue(this);
+        new SetValueTC(this);
 
-            this.getCommand("editbal").setExecutor(new EditBal(this));
-            this.getCommand("editbal").setTabCompleter(new EditBalTC(this));
+        // Enchant
+        new EnchantHandBuy(this);
+        new EnchantHandBuyTC(this);
+        new EnchantHandSell(this);
+        new EnchantHandSellTC(this);
+        new EnchantHandValue(this);
+        new EnchantHandValueTC(this);
+        new EnchantInfo(this);
+        new EnchantInfoTC(this);
+        new EnchantValue(this);
+        new EnchantValueTC(this);
 
-            this.getCommand("sendcash").setExecutor(new SendCash(this));
-            this.getCommand("sendcash").setTabCompleter(new SendCashTC(this));
+        // Help
+        new HelpCommand(this);
+        new HelpCommandTC(this);
 
-            this.getCommand("setbal").setExecutor(new SetBal(this));
-            this.getCommand("setbal").setTabCompleter(new SetBalTC(this));
+        // Mail
+        new ClearMail(this);
+        new ClearMailTC(this);
+        new ReadMail(this);
+        new ReadMailTC(this);
 
-            this.getCommand("clearbal").setExecutor(new ClearBal(this));
-            this.getCommand("clearbal").setTabCompleter(new ClearBalTC(this));
+        // Market
+        new Buy(this);
+        new BuyTC(this);
+        new HandBuy(this);
+        new HandBuyTC(this);
+        new HandInfo(this);
+        new HandSell(this);
+        new HandSellTC(this);
+        new HandValue(this);
+        new HandValueTC(this);
+        new Info(this);
+        new InfoTC(this);
+        new Sell(this);
+        new SellTC(this);
+        new Value(this);
+        new ValueTC(this);
 
-            this.getCommand("buy").setExecutor(new Buy(this));
-            this.getCommand("buy").setTabCompleter(new BuyTC(this));
+        // Misc
+        new Ping(this);
 
-            this.getCommand("sell").setExecutor(new Sell(this));
-            this.getCommand("sell").setTabCompleter(new SellTC(this));
-
-            this.getCommand("value").setExecutor(new Value(this));
-            this.getCommand("value").setTabCompleter(new ValueTC(this));
-
-            this.getCommand("information").setExecutor(new Info(this));
-            this.getCommand("information").setTabCompleter(new InfoTC(this));
-
-            this.getCommand("handSell").setExecutor(new HandSell(this));
-            this.getCommand("handSell").setTabCompleter(new HandSellTC(this));
-
-            this.getCommand("handBuy").setExecutor(new HandBuy(this));
-            this.getCommand("handBuy").setTabCompleter(new HandBuyTC(this));
-
-            this.getCommand("handValue").setExecutor(new HandValue(this));
-            this.getCommand("handValue").setTabCompleter(new HandValueTC(this));
-
-            this.getCommand("handInformation").setExecutor(new HandInfo(this));
-
-            this.getCommand("readMail").setExecutor(new ReadMail(this));
-            this.getCommand("readMail").setTabCompleter(new ReadMailTC(this));
-
-            this.getCommand("clearMail").setExecutor(new ClearMail(this));
-            this.getCommand("clearMail").setTabCompleter(new ClearMailTC(this));
-
-            this.getCommand("eSell").setExecutor(new EnchantHandSell(this));
-            this.getCommand("eSell").setTabCompleter(new EnchantHandSellTC(this));
-
-            this.getCommand("eValue").setExecutor(new EnchantValue(this));
-            this.getCommand("eValue").setTabCompleter(new EnchantValueTC(this));
-
-            this.getCommand("eHandValue").setExecutor(new EnchantHandValue(this));
-            this.getCommand("eHandValue").setTabCompleter(new EnchantHandValueTC(this));
-
-            this.getCommand("eBuy").setExecutor(new EnchantHandBuy(this));
-            this.getCommand("eBuy").setTabCompleter(new EnchantHandBuyTC(this));
-
-            this.getCommand("eInfo").setExecutor(new EnchantInfo(this));
-            this.getCommand("eInfo").setTabCompleter(new EnchantInfoTC(this));
-
-            this.getCommand("reloadMaterials").setExecutor(new ReloadMaterials(this));
-
-            this.getCommand("reloadEnchants").setExecutor(new ReloadEnchants(this));
-
-            this.getCommand("saveMaterials").setExecutor(new SaveMaterials(this));
-
-            this.getCommand("saveEnchants").setExecutor(new SaveEnchants(this));
-
-            this.getCommand("setStock").setExecutor(new SetStock(this));
-            this.getCommand("setStock").setTabCompleter(new SetStockTC(this));
-
-            this.getCommand("setValue").setExecutor(new SetValue(this));
-            this.getCommand("setValue").setTabCompleter(new SetValueTC(this));
-
-            this.getCommand("eSetStock").setExecutor(new ESetStock(this));
-            this.getCommand("eSetStock").setTabCompleter(new ESetStockTC(this));
-
-            this.getCommand("eSetValue").setExecutor(new ESetValue(this));
-            this.getCommand("eSetValue").setTabCompleter(new ESetValueTC(this));
-
-            this.getCommand("ehelp").setExecutor(new HelpCommand(this));
-            this.getCommand("ehelp").setTabCompleter(new HelpCommandTC(this));
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            this.console.severe("An error occurred on registry: " + e);
-            this.shutdown();
-            return;
-        }
+        // Money
+        new Balance(this);
+        new BalanceTC(this);
+        new SendCash(this);
+        new SendCashTC(this);
 
         // Done :)
         this.describe();
@@ -211,16 +183,16 @@ public class DEPlugin extends JavaPlugin {
         this.console.debug("Settings:");
         for (Setting setting : Setting.values()) {
             Object value = this.getConfigManager().get(setting);
-            if (!(value instanceof MemorySection)) this.getConsole().debug(String.format("   - %s: '%s'", setting.path, value));
+            if (!(value instanceof MemorySection)) this.getConsole().debug("   - %s: '%s'", setting.path, value);
         }
         this.console.debug("");
         this.console.debug("Markets:");
-        this.console.debug("   - Materials: " + this.materialManager.materials.size());
-        this.console.debug("      - Material Market Size: " + this.materialManager.getTotalMaterials() + " / " + this.materialManager.getDefaultTotalMaterials());
-        this.console.debug("      - Material Market Inflation: " + this.materialManager.getInflation() + "%");
-        this.console.debug("   - Enchants: " + this.enchantmentManager.enchants.size());
-        this.console.debug("      - Enchant Market Size: " + this.enchantmentManager.getTotalEnchants() + " / " + this.enchantmentManager.getDefaultTotalEnchants());
-        this.console.debug("      - Enchant Market Inflation: " + this.enchantmentManager.getInflation() + "%");
+        this.console.debug("   - Materials: %s", this.materialManager.getMaterialCount());
+        this.console.debug("      - Material Market Size: %s / %s", this.materialManager.getTotalMaterials(), this.materialManager.getDefaultTotalMaterials());
+        this.console.debug("      - Material Market Inflation: %s%%", this.materialManager.getInflation());
+        this.console.debug("   - Enchants: %s", this.enchantmentManager.getEnchantCount());
+        this.console.debug("      - Enchant Market Size: %s / %s", this.enchantmentManager.getTotalEnchants(), this.enchantmentManager.getDefaultTotalEnchants());
+        this.console.debug("      - Enchant Market Inflation: %s%%", this.enchantmentManager.getInflation());
         this.console.debug("");
     }
 
