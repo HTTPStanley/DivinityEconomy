a22d8db04233d90ce60f49495b96a387fbbcfa4c
EDGRRRRs <everybodylovesedgrrrr@gmail.com>
2021-08-13 17:16:29 +0100

    Update MailEvent.java
    Improved mail join message.

--- src/me/edgrrrr/de/events/MailEvent.java
+++ src/me/edgrrrr/de/events/MailEvent.java
@@ -2,10 +2,14 @@ package me.edgrrrr.de.events;
 
 import me.edgrrrr.de.DEPlugin;
 import me.edgrrrr.de.mail.MailList;
+import me.edgrrrr.de.player.PlayerManager;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * A mail event for telling players if they have pending mail or not
@@ -22,9 +26,9 @@ public class MailEvent implements Listener {
         Player player = event.getPlayer();
         MailList mailList = this.app.getMailMan().getMailList(player.getUniqueId().toString());
         if (mailList.hasMail()) {
-            this.app.getConsole().info(player, "You have %s previously read and %s unread mail", mailList.getReadMail().size(), mailList.getUnreadMail().size());
+            this.app.getConsole().info(player, "You have %s economy notifications. View them with /readmail", mailList.getAllMail().size());
         } else {
-            this.app.getConsole().info(player, "You have no mail. ");
+            this.app.getConsole().info(player, "You have no economy notifications. ");
         }
     }
 }
