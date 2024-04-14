2df544e3874b8995a463e83c63b46ab661c16265
HTTPStanley <76664326+HTTPStanley@users.noreply.github.com>
2024-04-14 21:40:54 +0100

    Committing changes for src/me/edgrrrr/de/commands/money/SendCash.java

--- src/me/edgrrrr/de/commands/money/SendCash.java
+++ src/me/edgrrrr/de/commands/money/SendCash.java
@@ -3,6 +3,7 @@ package me.edgrrrr.de.commands.money;
 import me.edgrrrr.de.DEPlugin;
 import me.edgrrrr.de.commands.DivinityCommand;
 import me.edgrrrr.de.config.Setting;
+import me.edgrrrr.de.lang.LangEntry;
 import me.edgrrrr.de.response.EconomyTransferResponse;
 import me.edgrrrr.de.utils.Converter;
 import org.bukkit.OfflinePlayer;
@@ -39,29 +40,29 @@ public class SendCash extends DivinityCommand {
         switch (args.length) {
             case 2:
                 // Get online player
-                player2 = this.getMain().getPlayMan().getPlayer(args[0], false);
+                player2 = getMain().getPlayMan().getPlayer(args[0], false);
                 amount = Converter.getDouble(args[1]);
                 break;
 
             default:
-                this.getMain().getConsole().usage(sender, CommandResponse.InvalidNumberOfArguments.message, this.help.getUsages());
+                getMain().getConsole().usage(sender, LangEntry.GENERIC_InvalidNumberOfArguments.get(getMain()), this.help.getUsages());
                 return true;
         }
 
         // Ensure online or offline player exists.
         if (player2 == null) {
-            this.getMain().getConsole().send(sender, CommandResponse.InvalidPlayerName.defaultLogLevel, CommandResponse.InvalidPlayerName.message);
+            getMain().getConsole().send(sender, LangEntry.GENERIC_InvalidPlayerName.logLevel, LangEntry.GENERIC_InvalidPlayerName.get(getMain()));
             return true;
         }
 
         // Sendcash response
-        EconomyTransferResponse response = this.getMain().getEconMan().sendCash(sender, player2, amount);
+        EconomyTransferResponse response = getMain().getEconMan().sendCash(sender, player2, amount);
 
         // Handles console, message and mail
         if (response.isSuccess()) {
-            this.getMain().getConsole().logTransfer(sender, player2, amount);
+            getMain().getConsole().logTransfer(sender, player2, amount);
         } else {
-            this.getMain().getConsole().logFailedTransfer(sender, player2, amount, response.getErrorMessage());
+            getMain().getConsole().logFailedTransfer(sender, player2, amount, response.getErrorMessage());
         }
         return true;
     }
