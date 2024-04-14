3264c874962cdb2658850968e235ccd810206891
HTTPStanley <76664326+HTTPStanley@users.noreply.github.com>
2024-04-14 21:35:39 +0100

    Update PAPIMaterialTotalDefaultQuantity.java

--- src/me/edgrrrr/de/placeholders/expansions/PAPIMaterialTotalDefaultQuantity.java
+++ src/me/edgrrrr/de/placeholders/expansions/PAPIMaterialTotalDefaultQuantity.java
@@ -11,6 +11,6 @@ public class PAPIMaterialTotalDefaultQuantity extends DivinityExpansion {
 
     @Override
     public String getResult(OfflinePlayer player, String value) {
-        return String.format("%d", this.getMain().getMatMan().getDefaultTotalItems());
+        return String.format("%d", getMain().getMatMan().getDefaultTotalItems());
     }
 }
