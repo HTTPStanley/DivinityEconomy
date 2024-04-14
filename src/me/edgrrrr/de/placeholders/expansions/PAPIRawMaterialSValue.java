802f287e4867b295c84d68adef785fb08c441797
HTTPStanley <76664326+HTTPStanley@users.noreply.github.com>
2024-04-14 21:35:31 +0100

    Update PAPIRawMaterialSValue.java

--- src/me/edgrrrr/de/placeholders/expansions/PAPIRawMaterialSValue.java
+++ src/me/edgrrrr/de/placeholders/expansions/PAPIRawMaterialSValue.java
@@ -15,7 +15,7 @@ public class PAPIRawMaterialSValue extends DivinityExpansion {
     public String getResult(OfflinePlayer player, String value) {
         String material = value.replaceFirst(this.value, "$1");
         int amount = Converter.getInt(value.replaceFirst(this.value, "$2"));
-        MarketableMaterial marketableMaterial = this.getMain().getMarkMan().getItem(material);
+        MarketableMaterial marketableMaterial = getMain().getMarkMan().getItem(material);
         if (marketableMaterial != null)
             return String.format("%,.2f", marketableMaterial.getManager().getSellValue(marketableMaterial.getItemStacks(amount)).getValue());
         else return String.format("Unknown material '%s'", material);
