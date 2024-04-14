744cf1568d9a2277c6c83bd52990989f48554cfb
HTTPStanley <76664326+HTTPStanley@users.noreply.github.com>
2024-04-14 21:37:29 +0100

    Update PlayerJoin.java

--- src/me/edgrrrr/de/economy/events/PlayerJoin.java
+++ src/me/edgrrrr/de/economy/events/PlayerJoin.java
@@ -1,16 +1,20 @@
 package me.edgrrrr.de.economy.events;
 
+import me.edgrrrr.de.DEPlugin;
 import me.edgrrrr.de.console.Console;
 import me.edgrrrr.de.economy.DivinityEconomy;
+import me.edgrrrr.de.lang.LangEntry;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoin implements Listener {
+    private final DEPlugin main;
     private final DivinityEconomy divinityEconomy;
 
-    public PlayerJoin(DivinityEconomy divinityEconomy) {
+    public PlayerJoin(DEPlugin main, DivinityEconomy divinityEconomy) {
+        this.main = main;
         this.divinityEconomy = divinityEconomy;
     }
 
@@ -19,7 +23,7 @@ public class PlayerJoin implements Listener {
         Player player = event.getPlayer();
         if (!this.divinityEconomy.hasAccount(player)) {
             this.divinityEconomy.createPlayerAccount(player);
-            Console.get().debug("Player '%s' (%s) did not previously have an account, one has been created for them.", player.getName(), player.getUniqueId());
+            Console.get().debug(LangEntry.ECONOMY_CreatingPlayerFile.get(this.main), player.getName(), player.getUniqueId());
         }
     }
 }
