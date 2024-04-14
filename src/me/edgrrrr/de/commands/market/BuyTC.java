0d3bafb52363b811a0fef315dcfc937e1af59009
HTTPStanley <76664326+HTTPStanley@users.noreply.github.com>
2024-04-14 21:40:52 +0100

    Committing changes for src/me/edgrrrr/de/commands/market/BuyTC.java

--- src/me/edgrrrr/de/commands/market/BuyTC.java
+++ src/me/edgrrrr/de/commands/market/BuyTC.java
@@ -3,10 +3,12 @@ package me.edgrrrr.de.commands.market;
 import me.edgrrrr.de.DEPlugin;
 import me.edgrrrr.de.commands.DivinityCommandMaterialsTC;
 import me.edgrrrr.de.config.Setting;
+import me.edgrrrr.de.lang.LangEntry;
 import me.edgrrrr.de.market.items.materials.MarketableMaterial;
 import me.edgrrrr.de.utils.Converter;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -33,49 +35,48 @@ public class BuyTC extends DivinityCommandMaterialsTC {
     @Override
     public List<String> onPlayerTabCompleter(Player sender, String[] args) {
         String[] strings;
+        ArrayList<String> list = new ArrayList<>();
         MarketableMaterial marketableMaterial;
         switch (args.length) {
             // 1 args
             // return names of players starting with arg
-            case 1 -> strings = this.getMain().getMarkMan().getItemNames(args[0]).toArray(new String[0]);
+            case 1 -> strings = getMain().getMarkMan().getItemNames(args[0]).toArray(new String[0]);
 
 
             // 2 args
             // return max stack size for the material given
             case 2 -> {
-                marketableMaterial = this.getMain().getMarkMan().getItem(args[0]);
+                marketableMaterial = getMain().getMarkMan().getItem(args[0]);
                 int stackSize;
                 if (marketableMaterial != null) {
                     stackSize = marketableMaterial.getMaterial().getMaxStackSize();
-                    strings = new String[]{
-                            String.valueOf(stackSize),
-                            "max",
-                            String.valueOf(marketableMaterial.getAvailableSpace(sender))
-                    };
+                    list.add(String.valueOf(stackSize));
+                    LangEntry.W_max.addLang(getMain(), list);
+                    list.add(String.valueOf(marketableMaterial.getAvailableSpace(sender)));
+                    strings = list.toArray(new String[0]);
                 } else {
-                    strings = new String[]{
-                            "Unknown material."
-                    };
+                    LangEntry.MARKET_UnknownMaterial.addLang(getMain(), list);
+                    strings = list.toArray(new String[0]);
                 }
             }
 
             // 3 args
             // If uses clicks space after number, returns the value of the amount of item given
             case 3 -> {
-                marketableMaterial = this.getMain().getMarkMan().getItem(args[0]);
-                String value = "unknown.";
+                marketableMaterial = getMain().getMarkMan().getItem(args[0]);
+                String value = LangEntry.W_unknown.get(getMain());
                 if (marketableMaterial != null) {
-                    int amount;
-                    if (args[1].equalsIgnoreCase("max")) {
+                    int amount = 1;
+                    if (LangEntry.W_max.is(getMain(), args[1])) {
                         amount = marketableMaterial.getAvailableSpace(sender);
                     } else {
                         amount = Converter.getInt(args[1]);
                     }
-                    value = String.format("%s", this.getMain().getConsole().formatMoney(marketableMaterial.getManager().calculatePrice(amount, marketableMaterial.getQuantity(), marketableMaterial.getManager().getBuyScale(), true)));
+                    value = String.format("%s", getMain().getConsole().formatMoney(marketableMaterial.getManager().calculatePrice(amount, marketableMaterial.getQuantity(), marketableMaterial.getManager().getBuyScale(), true)));
                 }
 
                 strings = new String[]{
-                        String.format("Value: %s", value)
+                        LangEntry.VALUE_Response.get(getMain(), value)
                 };
             }
 
