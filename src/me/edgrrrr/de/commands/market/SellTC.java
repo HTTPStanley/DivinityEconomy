ba81c87c61180fd96039ffdf45751ec445952418
HTTPStanley <76664326+HTTPStanley@users.noreply.github.com>
2024-04-14 21:40:53 +0100

    Committing changes for src/me/edgrrrr/de/commands/market/SellTC.java

--- src/me/edgrrrr/de/commands/market/SellTC.java
+++ src/me/edgrrrr/de/commands/market/SellTC.java
@@ -3,6 +3,7 @@ package me.edgrrrr.de.commands.market;
 import me.edgrrrr.de.DEPlugin;
 import me.edgrrrr.de.commands.DivinityCommandMaterialsTC;
 import me.edgrrrr.de.config.Setting;
+import me.edgrrrr.de.lang.LangEntry;
 import me.edgrrrr.de.market.items.materials.MarketableMaterial;
 import me.edgrrrr.de.player.PlayerManager;
 import me.edgrrrr.de.utils.Converter;
@@ -42,21 +43,21 @@ public class SellTC extends DivinityCommandMaterialsTC {
             // 1 args
             // return items in user inventory
             case 1 -> {
-                strings = this.getMain().getMarkMan().getItemNames(PlayerManager.getInventoryMaterialNames(sender), args[0]).toArray(new String[0]);
+                strings = getMain().getMarkMan().getItemNames(PlayerManager.getInventoryMaterialNames(sender), args[0]).toArray(new String[0]);
             }
 
             // 2 args
             // return amount in user inventory
             case 2 -> {
-                marketableMaterial = this.getMain().getMarkMan().getItem(args[0]);
+                marketableMaterial = getMain().getMarkMan().getItem(args[0]);
                 if (marketableMaterial == null) {
                     strings = new String[]{
-                            "Invalid material entered."
+                            LangEntry.MARKET_InvalidMaterialName.get(getMain())
                     };
                 } else {
                     Material material = marketableMaterial.getMaterial();
                     ArrayList<String> allStrings = new ArrayList<>();
-                    allStrings.add("max");
+                    LangEntry.W_max.addLang(getMain(), allStrings);
                     int stackSize = material.getMaxStackSize();
                     int inventoryCount = marketableMaterial.getMaterialCount(sender);
 
@@ -69,19 +70,19 @@ public class SellTC extends DivinityCommandMaterialsTC {
                 }
             }
             case 3 -> {
-                marketableMaterial = this.getMain().getMarkMan().getItem(args[0]);
-                String value = "unknown.";
+                marketableMaterial = getMain().getMarkMan().getItem(args[0]);
+                String value = LangEntry.W_unknown.get(getMain());
                 if (marketableMaterial != null) {
                     int amount;
-                    if (args[1].equalsIgnoreCase("max")) {
+                    if (LangEntry.W_max.is(getMain(), args[1])) {
                         amount = marketableMaterial.getAvailableSpace(sender);
                     } else {
                         amount = Converter.getInt(args[1]);
                     }
-                    value = this.getMain().getConsole().formatMoney(marketableMaterial.getManager().getSellValue(marketableMaterial.getMaterialSlotsToCount(sender, amount)).getValue());
+                    value = getMain().getConsole().formatMoney(marketableMaterial.getManager().getSellValue(marketableMaterial.getMaterialSlotsToCount(sender, amount)).getValue());
                 }
                 strings = new String[]{
-                        String.format("Value: %s", value)
+                        LangEntry.VALUE_Response.get(getMain(), value)
                 };
             }
             default -> strings = new String[0];
