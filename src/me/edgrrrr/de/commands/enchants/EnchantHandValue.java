e18901cc3b2ad6faaa53fd33edc96059586c4e4d
HTTPStanley <76664326+HTTPStanley@users.noreply.github.com>
2024-04-14 21:40:50 +0100

    Committing changes for src/me/edgrrrr/de/commands/enchants/EnchantHandValue.java

--- src/me/edgrrrr/de/commands/enchants/EnchantHandValue.java
+++ src/me/edgrrrr/de/commands/enchants/EnchantHandValue.java
@@ -3,6 +3,7 @@ package me.edgrrrr.de.commands.enchants;
 import me.edgrrrr.de.DEPlugin;
 import me.edgrrrr.de.commands.DivinityCommandEnchant;
 import me.edgrrrr.de.config.Setting;
+import me.edgrrrr.de.lang.LangEntry;
 import me.edgrrrr.de.market.MarketableToken;
 import me.edgrrrr.de.market.items.enchants.EnchantValueResponse;
 import me.edgrrrr.de.market.items.enchants.MarketableEnchant;
@@ -40,38 +41,38 @@ public class EnchantHandValue extends DivinityCommandEnchant {
         // ensure it is not null
         ItemStack heldItem = PlayerManager.getHeldItem(sender);
         if (heldItem == null) {
-            this.getMain().getConsole().usage(sender, CommandResponse.InvalidItemHeld.message, this.help.getUsages());
+            getMain().getConsole().usage(sender, LangEntry.MARKET_InvalidItemHeld.get(getMain()), this.help.getUsages());
             return true;
         }
 
         // Ensure item is enchanted
-        if (!this.getMain().getEnchMan().isEnchanted(heldItem)) {
-            this.getMain().getConsole().usage(sender, CommandResponse.InvalidItemHeld.message, this.help.getUsages());
+        if (!getMain().getEnchMan().isEnchanted(heldItem)) {
+            getMain().getConsole().usage(sender, LangEntry.MARKET_InvalidItemHeld.get(getMain()), this.help.getUsages());
             return true;
         }
 
 
         // Get evr1
-        EnchantValueResponse evr1 = this.getMain().getEnchMan().getBuyValue(heldItem, 0);
+        EnchantValueResponse evr1 = getMain().getEnchMan().getBuyValue(heldItem, 0);
         if (evr1.isFailure()) {
-            this.getMain().getConsole().warn(sender, "Couldn't determine buy value of %d enchants( %s ) because %s", evr1.getQuantity(), evr1.listNames(), evr1.getErrorMessage());
+            getMain().getConsole().warn(sender, LangEntry.PURCHASE_ValueEnchantFailure.get(getMain()), evr1.getQuantity(), evr1.listNames(), evr1.getErrorMessage());
         }
 
-        this.getMain().getConsole().info(sender, "Buy: %d enchants costs %s", evr1.getQuantity(), this.getMain().getConsole().formatMoney(evr1.getValue()));
+        getMain().getConsole().info(sender, LangEntry.PURCHASE_ValueEnchantSummary.get(getMain()), evr1.getQuantity(), getMain().getConsole().formatMoney(evr1.getValue()));
         for (MarketableToken token1 : evr1.getTokens()) {
             MarketableEnchant enchant1 = (MarketableEnchant) token1;
-            this.getMain().getConsole().info(sender, "  -Buy: %d %s costs %s", evr1.getQuantity(enchant1), enchant1.getCleanName(), this.getMain().getConsole().formatMoney(evr1.getValue(enchant1)));
+            getMain().getConsole().info(sender, LangEntry.PURCHASE_ValueEnchant.get(getMain()), evr1.getQuantity(enchant1), enchant1.getName(), getMain().getConsole().formatMoney(evr1.getValue(enchant1)));
         }
 
-        EnchantValueResponse evr2 = this.getMain().getEnchMan().getSellValue(heldItem, 0);
+        EnchantValueResponse evr2 = getMain().getEnchMan().getSellValue(heldItem, 0);
         if (evr2.isFailure()) {
-            this.getMain().getConsole().warn(sender, "Couldn't determine sell value of %d enchants( %s ) because %s", evr2.getQuantity(), evr2.listNames(), evr2.getErrorMessage());
+            getMain().getConsole().warn(sender, LangEntry.SALE_ValueEnchantFailure.get(getMain()), evr2.getQuantity(), evr2.listNames(), evr2.getErrorMessage());
         }
 
-        this.getMain().getConsole().info(sender, "Sell: %d enchants costs %s", evr2.getQuantity(), this.getMain().getConsole().formatMoney(evr2.getValue()));
+        getMain().getConsole().info(sender, LangEntry.SALE_ValueEnchantSummary.get(getMain()), evr2.getQuantity(), getMain().getConsole().formatMoney(evr2.getValue()));
         for (MarketableToken token2 : evr2.getTokens()) {
             MarketableEnchant enchant2 = (MarketableEnchant) token2;
-            this.getMain().getConsole().info(sender, "  -Sell: %d %s costs %s", evr2.getQuantity(enchant2), enchant2.getCleanName(), this.getMain().getConsole().formatMoney(evr2.getValue(enchant2)));
+            getMain().getConsole().info(sender, LangEntry.SALE_ValueEnchant.get(getMain()), evr2.getQuantity(enchant2), enchant2.getName(), getMain().getConsole().formatMoney(evr2.getValue(enchant2)));
         }
 
         return true;
