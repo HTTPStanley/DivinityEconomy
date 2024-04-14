50c3aeb4804944a5985a373f09d6a92151fb0db1
HTTPStanley <76664326+HTTPStanley@users.noreply.github.com>
2024-04-14 21:40:50 +0100

    Committing changes for src/me/edgrrrr/de/commands/enchants/EnchantHandSellTC.java

--- src/me/edgrrrr/de/commands/enchants/EnchantHandSellTC.java
+++ src/me/edgrrrr/de/commands/enchants/EnchantHandSellTC.java
@@ -3,6 +3,7 @@ package me.edgrrrr.de.commands.enchants;
 import me.edgrrrr.de.DEPlugin;
 import me.edgrrrr.de.commands.DivinityCommandEnchantTC;
 import me.edgrrrr.de.config.Setting;
+import me.edgrrrr.de.lang.LangEntry;
 import me.edgrrrr.de.player.PlayerManager;
 import me.edgrrrr.de.utils.Converter;
 import org.bukkit.Material;
@@ -38,30 +39,35 @@ public class EnchantHandSellTC extends DivinityCommandEnchantTC {
     public List<String> onPlayerTabCompleter(Player sender, String[] args) {
         String[] strings;
         ItemStack heldItem = PlayerManager.getHeldItem(sender, new ItemStack(Material.AIR, 0));
+        String max = LangEntry.W_max.get(getMain());
+        String defaultMax = LangEntry.W_max.get(getMain());
         switch (args.length) {
             // 1 args
             // return names of players starting with arg
             case 1 -> {
                 ArrayList<String> allStrings = new ArrayList<>();
-                allStrings.add("max");
-                allStrings.addAll(this.getMain().getEnchMan().getItemNames(heldItem, args[0]));
+                allStrings.add(defaultMax);
+                if (!max.equalsIgnoreCase(defaultMax)) {
+                    allStrings.add(max);
+                }
+                allStrings.addAll(getMain().getEnchMan().getItemNames(heldItem, args[0]));
                 strings = allStrings.toArray(new String[0]);
             }
 
             // 2 args
             // return max stack size for the material given
             case 2 -> {
-                if (args[0].equalsIgnoreCase("max")) {
-                    strings = new String[]{this.getMain().getEnchMan().getSellValueString(heldItem)};
+                if (args[0].equalsIgnoreCase(max) || args[0].equalsIgnoreCase(defaultMax)) {
+                    strings = new String[]{getMain().getEnchMan().getSellValueString(heldItem)};
                 } else {
-                    strings = this.getMain().getEnchMan().getDowngradeValueString(heldItem, args[0]);
+                    strings = getMain().getEnchMan().getDowngradeValueString(heldItem, args[0]);
                 }
             }
 
             // 3 args
             // If uses clicks space after number, returns the value of the amount of item given
             case 3 -> {
-                strings = new String[]{this.getMain().getEnchMan().getSellValueString(heldItem, args[0], Converter.getInt(args[1]))};
+                strings = new String[]{getMain().getEnchMan().getSellValueString(heldItem, args[0], Converter.getInt(args[1]))};
             }
 
             // else
