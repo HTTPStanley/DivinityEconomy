66db53e6e6a496bde8ffca9d5636e40191786dbc
HTTPStanley <76664326+HTTPStanley@users.noreply.github.com>
2024-04-14 21:40:47 +0100

    Committing changes for src/me/edgrrrr/de/commands/DivinityCommandMaterialsTC.java

--- src/me/edgrrrr/de/commands/DivinityCommandMaterialsTC.java
+++ src/me/edgrrrr/de/commands/DivinityCommandMaterialsTC.java
@@ -21,7 +21,7 @@ public abstract class DivinityCommandMaterialsTC extends DivinityCommandTC {
      */
     public DivinityCommandMaterialsTC(DEPlugin app, String registeredCommandName, boolean hasConsoleSupport, Setting commandSetting) {
         super(app, registeredCommandName, hasConsoleSupport, commandSetting);
-        this.marketIsEnabled = this.getMain().getConfig().getBoolean(Setting.MARKET_MATERIALS_ENABLE_BOOLEAN.path);
+        this.marketIsEnabled = getMain().getConfig().getBoolean(Setting.MARKET_MATERIALS_ENABLE_BOOLEAN.path);
     }
 
     /**
