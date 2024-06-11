09f677ad3fa8da3cf960db6399e6e12846e3465b
HTTPStanley <76664326+HTTPStanley@users.noreply.github.com>
2024-06-11 16:01:08 +0100

    Update HandBuy.java

--- src/me/edgrrrr/de/commands/market/HandBuy.java
+++ src/me/edgrrrr/de/commands/market/HandBuy.java
@@ -74,7 +74,7 @@ public class HandBuy extends DivinityCommandMaterials {
             getMain().getConsole().send(sender, LangEntry.MARKET_InvalidItemHeld.logLevel, LangEntry.MARKET_InvalidItemHeld.get(getMain()));
             return true;
         }
-        
+
         int availableSpace = marketableMaterial.getAvailableSpace(sender);
 
         // Ensure the material is allowed to be bought and sold
