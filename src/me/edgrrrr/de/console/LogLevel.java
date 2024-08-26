a722a17e3634220d2247382db376ce008eff7e3b
HTTPStanley <76664326+HTTPStanley@users.noreply.github.com>
2024-08-26 18:19:51 +0100

    Update LogLevel.java

--- src/me/edgrrrr/de/console/LogLevel.java
+++ src/me/edgrrrr/de/console/LogLevel.java
@@ -7,34 +7,36 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import java.util.logging.Logger;
 
 public enum LogLevel {
-    DEBUG(0, ChatColor.DARK_PURPLE, Setting.CHAT_DEBUG_COLOR.path),
-    INFO(1, ChatColor.GREEN, Setting.CHAT_INFO_COLOR.path),
-    WARNING(2, ChatColor.YELLOW, Setting.CHAT_WARNING_COLOR.path),
-    SEVERE(3, ChatColor.DARK_RED, Setting.CHAT_SEVERE_COLOR.path);
+    DEBUG(ChatColor.DARK_PURPLE, Setting.CHAT_DEBUG_COLOR.path),
+    INFO(ChatColor.GREEN, Setting.CHAT_INFO_COLOR.path),
+    WARNING(ChatColor.YELLOW, Setting.CHAT_WARNING_COLOR.path),
+    SEVERE(ChatColor.DARK_RED, Setting.CHAT_SEVERE_COLOR.path),
+    MIGRATE(ChatColor.GOLD, null);
 
-    private final int priority;
     private ChatColor colour;
     private final String colourOption;
 
-    LogLevel(int priority, ChatColor color, String colourOption) {
-        this.priority = priority;
+    LogLevel(ChatColor color, String colourOption) {
         this.colour = color;
         this.colourOption = colourOption;
     }
 
     public static void loadValuesFromConfig(YamlConfiguration config) {
         for (LogLevel level : values()) {
-            String value = config.getString(level.getColourOption());
+            // Skip migrate level
+            String settingKey = level.getColourOption();
+            if (settingKey == null) {
+                continue;
+            }
+
+            // Load the colour from the config
+            String value = config.getString(settingKey);
             try {
                 level.setColour(ChatColor.valueOf(value));
             } catch (Exception e) {
                 Logger.getLogger("Minecraft").severe(String.format("Exception occurred on log level loading (%s): %s", level, e.getMessage()));
             }
         }
-    }
-
-    public int getPriority() {
-        return priority;
     }
 
     public ChatColor getColour() {
@@ -47,9 +49,5 @@ public enum LogLevel {
 
     public String getColourOption() {
         return colourOption;
-    }
-
-    public boolean hasPriority(LogLevel over) {
-        return priority > over.priority;
     }
 }
