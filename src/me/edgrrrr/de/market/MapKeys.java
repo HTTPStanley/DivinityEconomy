76f3bc5529b4bcd3f38ff03150deea9f8be31cd0
HTTPStanley <76664326+HTTPStanley@users.noreply.github.com>
2024-08-26 10:50:26 +0100

    Update MapKeys.java

--- src/me/edgrrrr/de/market/MapKeys.java
+++ src/me/edgrrrr/de/market/MapKeys.java
@@ -4,11 +4,9 @@ public enum MapKeys {
     ALLOWED("ALLOWED"),
     QUANTITY("QUANTITY"),
     MATERIAL_ID("MATERIAL_ID"),
-    UPGRADED("POTION_UPGRADED"),
     POTION_TYPE("POTION_TYPE"),
     MAX_LEVEL("MAX_LEVEL"),
     ENCHANT_ID("ENCHANT_ID"),
-    EXTENDED("POTION_EXTENDED"),
     ENTITY_ID("ENTITY_ID");
     public final String key;
 
